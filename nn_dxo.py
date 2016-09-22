5d0f30e07b5c8d068a21ae47014f23832445c2a9
Håkon Treider <haakonvt@gmail.com>
2016-09-22 21:05:25 +0200

    Added safety check of nn, where random data (consistent) is used in stead

--- nn_dxo.py
+++ nn_dxo.py
@@ -1,42 +1,28 @@
 """
 Train a neural network to approximate a function from DXOMARK.com
-"""
-
-import tensorflow as tf
-import numpy as np
-import sys
-import datetime as time
-import os
-import shutil
-import matplotlib.pyplot as plt
-
-import neuralNetworkXavier as nnx
+that decides the "Global Score" value.
 
 # Data: https://www.dxomark.com/cameras#hideAdvancedOptions=false&viewMode=list&yDataType=rankDxo
 # Extracted data: https://regex101.com/r/vF9pS7/1
+"""
+import neuralNetworkXavier as nnx
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import datetime as time
+import numpy as np
+import shutil, sys, os
 
+""" First time run:
 x = np.loadtxt("values_dxo.dat"); y = len(x)/4
 x = x.reshape((y,4))
 
 totalDataPoints = x.shape[0] #(323, 4)
 
 np.savetxt("values_array_dxo.dat",x,delimiter=',\t',fmt='%.1f')
-
-# Pick out test data randomly from the data
-randRows = np.random.choice(totalDataPoints, 80, replace=False)
-global xRand
-xRand = x[randRows,:]
-
-# Pick out the rest for training data
-leftoverRows = [i for i in range(totalDataPoints) if i not in randRows]
-
-global trainData
-trainData = x[leftoverRows,:]
-np.random.shuffle(trainData) # Shuffle rows of the data to minimize impact of ordering of the data
+np.save("raw_data_dxo",x)"""
 
 def functionData(trainSize,testSize):
     """
-    RIGHT:
     Create train input for NN and test data
     """
     x_train = trainData[:,1:]
@@ -50,36 +36,34 @@ def functionData(trainSize,testSize):
     y_test = y_test.reshape([testSize,1])
     return x_train, y_train, x_test, y_test
 
-# reset so that variables are not given new names
-tf.reset_default_graph()
 
-# number of samples
-trainSize = 323-80
+def functionDataCreated(trainSize,testSize):
+    """Test the neural network with data generated randomly, with approximately the same
+    distribution, but with a known function to find the Global Score"""
+    score = lambda x,y,z: np.around((98/3.0) * (x/float(26.5) + y/float(14.8) + z/float(3702)), decimals=1)
 
-batchSize = 323-80
-testSize = 80
+    xRandArray = np.around(np.random.uniform(17.9, 26.5,   trainSize), decimals=1)
+    yRandArray = np.around(np.random.uniform(9.6,  14.8,   trainSize), decimals=1)
+    zRandArray = np.around(np.random.uniform(68.0, 3702.0, trainSize), decimals=1)
 
-# get random input
-xTrain, yTrain, xTest, yTest = functionData(trainSize,testSize)
+    x_train = np.column_stack((xRandArray,yRandArray,zRandArray))
+    y_train = score(xRandArray, yRandArray, zRandArray)
+    y_train = y_train.reshape([trainSize,1])
 
-# number of inputs and outputs
-inputs  = 3
-outputs = 1
+    xRandArray = np.around(np.random.uniform(17.9, 26.5,   testSize), decimals=1)
+    yRandArray = np.around(np.random.uniform(9.6,  14.8,   testSize), decimals=1)
+    zRandArray = np.around(np.random.uniform(68.0, 3702.0, testSize), decimals=1)
 
-x = tf.placeholder('float', [None, inputs],  name="x")
-y = tf.placeholder('float', [None, outputs], name="y")
+    x_test = np.column_stack((xRandArray,yRandArray,zRandArray))
+    y_test = score(xRandArray, yRandArray, zRandArray)
+    y_test = y_test.reshape([testSize,1])
+    return x_train, y_train, x_test, y_test
 
 
-neuralNetwork = lambda data : nnx.modelSigmoid(data, nNodes=nNodes, hiddenLayers=hiddenLayers,
-                                               wInitMethod='normal', bInitMethod='normal')
-
-def train_neural_network(x,epochs, nNodes,hiddenLayers, plot=False, no_print=False, learning_rate_choice=0.001):
-
+def train_neural_network(x, epochs, nNodes, hiddenLayers, plot=False, no_print=False, learning_rate_choice=0.001):
     # begin session
     with tf.Session() as sess:
-
         # pass data to network and receive output
-        #prediction = neuralNetwork(x)
         prediction, weights, biases, neurons = neuralNetwork(x)
 
         cost = tf.nn.l2_loss( tf.sub(prediction, y) )
@@ -87,14 +71,15 @@ def train_neural_network(x,epochs, nNodes,hiddenLayers, plot=False, no_print=Fal
         optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate_choice).minimize(cost)
 
         # number of cycles of feed-forward and backpropagation
-        numberOfEpochs = epochs; bestEpochTestLoss = -1
+        numberOfEpochs    = epochs;
+        bestEpochTestLoss = -1
 
         # initialize variables or restore from file
         saver = tf.train.Saver(weights + biases, max_to_keep=None)
         sess.run(tf.initialize_all_variables())
-        """if loadFlag:
+        if loadFlag:
             saver.restore(sess, loadFileName)
-            print 'Model restored'"""
+            print 'Model restored'
 
 
         bestTrainLoss = 1E100; bestTestLoss = 1E100
@@ -109,14 +94,13 @@ def train_neural_network(x,epochs, nNodes,hiddenLayers, plot=False, no_print=Fal
             if no_print:
                 pass
             else:
-                pass
-                #if (epoch+1)%int(numberOfEpochs/100.) == 0:
-                #    print 'Epoch %5d out of %5d trainloss/N: %10g, testloss/N: %10g' % \
-                #          (epoch+1, numberOfEpochs, epochLoss/float(trainSize), testCost/float(testSize))
+                if (epoch+1)%int(numberOfEpochs/100.) == 0:
+                    print 'Epoch %5d out of %5d trainloss/N: %10g, testloss/N: %10g' % \
+                          (epoch+1, numberOfEpochs, epochLoss/float(trainSize), testCost/float(testSize))
 
             """if epochLoss < bestTrainLoss and epochLoss/float(trainSize) < 20.0:
                 bestTrainLoss = epochLoss"""
-            if testCost < bestTestLoss and testCost//float(testSize) < 1.0:
+            """if testCost < bestTestLoss and testCost//float(testSize) < 1.0:
                 bestTestLoss = testCost
                 bestEpochTestLoss = epoch
                 print 'Epoch %5d out of %5d trainloss/N: %10g, testloss/N: %10g' % \
@@ -124,10 +108,11 @@ def train_neural_network(x,epochs, nNodes,hiddenLayers, plot=False, no_print=Fal
                 if plot:
                     yy = sess.run(prediction, feed_dict={x: xTest})
                     error = yy-yTest
-                    #break
+                    #break"""
 
         if plot:
-            #yy = sess.run(prediction, feed_dict={x: xTest})
+            yy = sess.run(prediction, feed_dict={x: xTest})
+            error = yy-yTest
             try:
                 error = yy-yTest
             except:
@@ -135,17 +120,74 @@ def train_neural_network(x,epochs, nNodes,hiddenLayers, plot=False, no_print=Fal
             plt.hist(error,bins=13)
             plt.xlabel('Test case (number)')
             plt.ylabel('Error: Prediciton - Exact answer')
-            np.savetxt("nndxo_err.dat",error)
+            np.savetxt("nndxo_err.dat",(yy, yTest, error))
             plt.show()
 
 
     return weights, biases, neurons, epochLoss/trainSize
 
 
-
+#--------------#
 ##### main #####
-print "---------------------------------------"
+#--------------#
+raw_data = np.load("raw_data_dxo.npy")
+cmdArgs  = len(sys.argv)-1
+if cmdArgs < 1: # No input from user, running default
+    pass
+else: # We need to parse the command line args
+    global saveFlag; global loadFlag; global plotFlag
+    saveFlag, loadFlag, plotFlag = False, False, False
+    for argIndex in range(1,cmdArgs+1):
+        #print "Index:",argIndex, "content:", sys.argv[argIndex]
+        arg = sys.argv[argIndex]
+        if arg == "-save":
+            saveFlag = True
+            print "The neural network (weights & biases) will be saved periodically"
+        if arg == "-load":
+            loadFlag = True
+            print "Loading latest neural network as starting point"
+        if arg == "-plot":
+            loadFlag = True
+        if arg in ["h","help","-h","-help","--h","--help"]:
+            print "Command line arguments possible: -save, -load and -plot. Exiting..."
+            sys.exit()
 
+# Pick out test data randomly from the data
+randRows = np.random.choice(totalDataPoints, 80, replace=False)
+global xRand
+xRand = x[randRows,:]
+
+# Pick out the rest for training data
+leftoverRows = [i for i in range(totalDataPoints) if i not in randRows]
+
+global trainData
+trainData = x[leftoverRows,:]
+np.random.shuffle(trainData) # Shuffle rows of the data to minimize impact of ordering of the data
+
+# reset so that variables are not given new names
+tf.reset_default_graph()
+
+# number of samples
+trainSize = len(trainData)
+batchSize = len(trainData)
+testSize  = 80 # Out of a hat, we found the number 80
+
+# get real world input
+#xTrain, yTrain, xTest, yTest = functionData(trainSize,testSize)
+# get random input
+xTrain, yTrain, xTest, yTest = functionDataCreated(trainSize,testSize)
+
+# number of inputs and outputs
+inputs  = 3
+outputs = 1
+
+x = tf.placeholder('float', [None, inputs],  name="x")
+y = tf.placeholder('float', [None, outputs], name="y")
+
+neuralNetwork = lambda data : nnx.modelSigmoid(data, nNodes=nNodes, hiddenLayers=hiddenLayers,
+                                               wInitMethod='normal', bInitMethod='normal')
+
+print "---------------------------------------"
 learning_rate_choice = 0.001 # Default for AdamOptimizer is 0.001
 testCases = 0;
 print "Learning rate:", learning_rate_choice
@@ -158,7 +200,7 @@ for hiddenLayers in [10]:
         for epochs in [100000]:
             testCases += 1
             weights, biases, neurons, epochlossPerN = train_neural_network(x, epochs, \
-                    nNodes, hiddenLayers,plot=True,no_print=False,learning_rate_choice=learning_rate_choice)
+                    nNodes, hiddenLayers,plot=plotFlag,no_print=False,learning_rate_choice=learning_rate_choice)
             print "\nHid.layers: %2.d, nodes/l: %2.d, epochs: %d, loss/N: %f" %(hiddenLayers,nNodes,epochs,epochlossPerN)
             if epochlossPerN < epochlossPerNPrev:
                 epochlossPerNPrev = epochlossPerN
