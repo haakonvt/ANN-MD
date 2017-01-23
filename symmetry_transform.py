d894814ab53f53f551a9edbc4114e60723fc1fb4
Håkon Treider <haakonvt@gmail.com>
2017-01-23 10:57:37 +0100

    Symmetry functions done, rdy 4 testing

--- symmetry_transform.py
+++ symmetry_transform.py
@@ -96,12 +96,24 @@ def G4(xyz, rc, eta, zeta, lambda_c, cutoff):
     return
 
 
-def G5(r, rc, cutoff):
+def G5(xyz, rc, eta, zeta, lambda_c, cutoff):
+    """ xyz:
+    [[x1 y1 z1]
+     [x2 y2 z2]
+     [x3 y3 z3]
+     [x4 y4 z4]]
+     """
+    r         = np.linalg.norm(xyz,axis=1)
     N         = len(r)
     r_cut     = cutoff(r)
-    summation = np.sum(r_cut)
+    summation = 0
     for j in range(N):
-        pass
+        for k in range(N):
+            theta_ijk  = acos( np.dot(xyz[j],xyz[k]) / (r[j]*r[k]) )
+            cutoff_ijk = r_cut[j]*r_cut[k]
+            part_sum   = (1+lambda_c * cos(theta_ijk))**zeta * exp(-eta*(r[j]**2+r[k]**2))
+            summation += part_sum*cutoff_ijk
+    summation *= 2**(1-zeta)
     return
 
 
@@ -114,45 +126,5 @@ if __name__ == '__main__':
     """
     Mainly for testing purpose
     """
-    # General plot settings:
-    colormap = plt.cm.Spectral #nipy_spectral # Other possible colormaps: Set1, Accent, nipy_spectral, Paired
 
-    if True: # Case one
-        N      = 6
-        colors = [colormap(i) for i in np.linspace(0, 1, N)]
-        r    = np.linspace(0,3,10001)
-        for i,rc in enumerate(np.linspace(0.75,2.75,N)):
-            r_cut = cutoff_cos(r,rc)
-            plt.plot(r,r_cut,color=colors[i])
-            plt.title("Case 1")
-        plt.show()
-
-    if True: # Case one
-        N      = 6
-        colors = [colormap(i) for i in np.linspace(0, 1, N)]
-        r    = np.linspace(0,3,10001)
-        for i,rc in enumerate(np.linspace(0.75,2.75,N)):
-            r_cut = the_differ(cutoff_cos(r,rc))
-            plt.plot(r[:-2],r_cut,color=colors[i])
-            plt.title("Case 2")
-        plt.show()
-
-    if True: # Case two
-        N      = 6
-        colors = [colormap(i) for i in np.linspace(0, 1, N)]
-        r    = np.linspace(0,3,10001)
-        for i,rc in enumerate(np.linspace(0.75,2.75,N)):
-            r_cut = cutoff_tanh(r,rc)
-            plt.plot(r,r_cut,color=colors[i])
-            plt.title("Case 3")
-        plt.show()
-
-    if True: # Case two
-        N      = 6
-        colors = [colormap(i) for i in np.linspace(0, 1, N)]
-        r    = np.linspace(0,3,10001)
-        for i,rc in enumerate(np.linspace(0.75,2.75,N)):
-            r_cut = the_differ(cutoff_tanh(r,rc))
-            plt.plot(r[:-2],r_cut,color=colors[i])
-            plt.title("Case 4")
-        plt.show()
+    print "This does absolutely nothing, I'm afraid dear!"
