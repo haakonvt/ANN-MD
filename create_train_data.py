e87bc59727f3d5920d14014c546a9d77836c98ea
Håkon Treider <haakonvt@gmail.com>
2017-02-06 23:16:25 +0100

    Cleanup complete, SW soon finished

--- create_train_data.py
+++ create_train_data.py
@@ -1,80 +1,68 @@
 """
-Train a neural network to approximate a 2-variable function
+Train a neural network to approximate a potential energy surface
+with the use of symmetry functions that transform xyz-data.
 """
+# from symmetry_functions import G1, G2, G3, G4, G5
+from symmetry_transform import symmetryTransform
 from mpl_toolkits.mplot3d import Axes3D
+from math import pi,sqrt,acos,exp,cos
 import neural_network_setup as nns
 import matplotlib.pyplot as plt
-from math import pi,sqrt,acos
 import matplotlib.cm as cm
 import tensorflow as tf
 import numpy as np
-import warnings
 import sys,os
 import glob
 
-def trainOrTest(size):# Cube object to be returned
-    nInputs = np.zeros((size,neighbors,4))
-    xyz     = np.zeros((size,3))
-    for i in range(neighbors): # Fill cube slice for each neighbor
-        nInputs[:,i,3] = np.random.uniform(0.8, 2.5, size) # this is R
-        r2             = nInputs[:,i,3]**2
-        xyz[:,0]       = np.random.uniform(0, r2, size)
-        xyz[:,1]       = np.random.uniform(0, r2-xyz[:,0], size)
-        xyz[:,2]       = r2 - xyz[:,0] - xyz[:,1]
-        for row in range(size):
-            np.random.shuffle(xyz[row,:]) # This shuffles in-place (so no copying)
-        nInputs[:,i,0] = np.sqrt(xyz[:,0]) * np.random.choice([-1,1],size) # 50-50 if position is plus or minus
-        nInputs[:,i,1] = np.sqrt(xyz[:,1]) * np.random.choice([-1,1],size)
-        nInputs[:,i,2] = np.sqrt(xyz[:,2]) * np.random.choice([-1,1],size)
-        # plt.subplot(3,1,1);plt.hist(nInputs[:,:,0].ravel(),bins=70);plt.subplot(3,1,2);plt.hist(nInputs[:,:,1].ravel(),bins=70);plt.subplot(3,1,3);plt.hist(nInputs[:,:,2].ravel(),bins=70);plt.show()
-    return nInputs
+def potentialEnergyGenerator(xyz_N, PES):
+    size = xyz_N.shape[2]
+    Ep   = np.zeros(size)
+    # r    = np.linalg.norm(xyz_N, axis=1)
 
-def outputGenerator(nInput):
-    size    = nInput.shape[0]
-    nOutput = np.zeros((size, 4)) # 4: Fx, Fy, Fz and Ep
-    r       = nInput[:,:,3]
-    # Sum up contribution from all neighbors:
-    nOutput[:,0] = np.sum( (FORCES(r) * nInput[:,:,0] / r) ,axis=1) # Fx
-    nOutput[:,1] = np.sum( (FORCES(r) * nInput[:,:,1] / r) ,axis=1) # Fy
-    nOutput[:,2] = np.sum( (FORCES(r) * nInput[:,:,2] / r) ,axis=1) # Fz
-    nOutput[:,3] = np.sum(PES( r ),axis=1) # Ep_tot
-    return nOutput
+    for i in range(size):
+        xyz_i = xyz_N[:,:,i]
+        Ep[i] = PES(xyz_i)
+    return Ep
 
-def createData_xyz(trainSize, testSize, PES, FORCES, neighbors=5):
+def createXYZ(r_min, r_max, size, neighbors=20, histogramPlot=False):
     """
-    Train with both potential and forces
-    # Input:  [x,   y,  z]
-    # Output: []
+    # Input:  Size of train and test size + number of neighbors
+    # Output: xyz-neighbors-matrix of size 'size'
     """
     if neighbors <= 1:
-        print "This code is meant for training a NN with more than one neighbor."
-        sys.exit()
+        print "This code is meant for training a NN with more than one neighbor."; sys.exit()
+    print "Creating XYZ-neighbor-data for:\n - Neighbors: %d \n - Samples  :%d" %(neighbors,size)
+    print "-------------------------------"
 
-    # Example
-    # PES    = lambda s:  1.0/s**12 - 1.0/s**6 # Lennard Jones pot. energy
-    # FORCES = lambda s: 12.0/s**13 - 6.0/s**7 # Analytic derivative of LJ with minus sign: F = -d/dx Ep
+    xyz_N = np.zeros((neighbors,3,size))
+    xyz   = np.zeros((size,3))
+    for i in range(neighbors): # Fill cube slice for each neighbor (quicker than "size")
+        r2       = np.random.uniform(r_min, r_max,       size)**2
+        xyz[:,0] = np.random.uniform(0,     r2,          size)
+        xyz[:,1] = np.random.uniform(0,     r2-xyz[:,0], size)
+        xyz[:,2] = r2 - xyz[:,0] - xyz[:,1]
+        for row in range(size):
+            np.random.shuffle(xyz[row,:]) # This shuffles in-place (so no copying)
+        xyz_N[i,0,:] = np.sqrt(xyz[:,0]) * np.random.choice([-1,1],size) # 50-50 if position is plus or minus
+        xyz_N[i,1,:] = np.sqrt(xyz[:,1]) * np.random.choice([-1,1],size)
+        xyz_N[i,2,:] = np.sqrt(xyz[:,2]) * np.random.choice([-1,1],size)
+    if histogramPlot:
+        plt.subplot(3,1,1);plt.hist(xyz_N[:,0,:].ravel(),bins=70);plt.subplot(3,1,2);plt.hist(xyz_N[:,1,:].ravel(),bins=70);plt.subplot(3,1,3);plt.hist(xyz_N[:,2,:].ravel(),bins=70);plt.show()
+    return xyz_N
 
-    # Generate train and test data
-    input_train  = trainOrTest(trainSize)
-    input_test   = trainOrTest(testSize)
-    output_train = outputGenerator(input_train)
-    output_test  = outputGenerator(input_test)
-
-
-    # # TODO: Not sure the reshape does what I intend (unravel second and last index)
-    # return input_train.reshape(trainSize,neighbors*4), output_train, input_test.reshape(testSize,neighbors*4), output_test
-
-def PES_Stillinger_Weber(xyz):
+def PES_Stillinger_Weber(xyz_i):
     """
     INPUT
-    - xyz: Matrix with columnds containing cartesian coordinates i.e.:
+    - xyz_i: Matrix with columnds containing cartesian coordinates,
+           relative to the current atom i, i.e.:
            [[x1 y1 z1]
             [x2 y2 z2]
             [x3 y3 z3]
             [x4 y4 z4]]
     """
+    xyz = xyz_i
     r = np.linalg.norm(xyz,axis=1)
-    N = len(r)
+    N = len(r) # Number of neighbors for atom i, which we are currently inspecting
 
     # A lot of definitions first
     A = 7.049556277
@@ -82,27 +70,51 @@ def PES_Stillinger_Weber(xyz):
     p = 4
     q = 0
     a = 1.8
-    l = 21  # [lambda]
-    g = 1.2 # [gamma]
-    cos_tc = -1.0/3.0
+    l = 21  # lambda
+    g = 1.2 # gamma
+    cos_tc = -1.0/3.0 # 109.47 deg
 
     eps = 2.1678  # [eV]
     m   = 28.085  # [amu]
     sig = 0.20951 # [nm]
     Ts  = 25156.73798 # eps/kB [K]
 
-    from math import exp,cos
     rc = (r < a) # Bool array. "False" cast to 0 and "True" to 1
     U2 = eps*A*(B*r**(-p)-r**(-q)) * np.exp(r-a)**-1 * rc
-    U3 = lambda rij, rik, theta_ijk: eps*l*exp(g/(rij-a) + g/(rik-a)) * (cos(theta_ijk) - cos_tc)**2 * (rij < a) * (rik < a)
+    U3 = lambda rij, rik, theta_ijk: eps*l*exp(g/(rij-a) + g/(rik-a)) \
+                                     * (cos(theta_ijk) - cos_tc)**2   \
+                                     * (rij < a) * (rik < a)
 
-    U = np.sum(U2)
+    U = np.sum(U2) # Two body terms
     for j in range(N): # i < j
         for k in range(j,N): # i < j < k
-            theta_ijk  = acos(np.dot(xyz[j],xyz[k]) / (r[j]*r[k]))
-            U         += U3(r[j], r[k], theta_ijk)
+            theta_ijk  = acos(np.dot(xyz[j],xyz[k]) / (r[j]*r[k])) # TODO: Angle sometimes wrong (abs > 1)
+            U         += U3(r[j], r[k], theta_ijk) # Three body terms
+    return U
 
+def FORCES_Stillinger_Weber(xyz_i):
+    """
+    To be implemented
+    """
+    pass
 
+def PES_Lennard_Jones(xyz_i):
+    """
+    Simple LJ pair potential
+    """
+    # eps = 1.0318 * 10^(-2) eV
+    # sig = 3.405 * 10^(-7) meter
+    xyz = xyz_i
+    r   = np.linalg.norm(xyz,axis=1)
+    U   =  np.sum(4*(1.0/r**12 - 1.0/r**6))
+    return U
+
+def FORCES_Lennard_Jones(xyz_i):
+    """
+    To be implemented:
+    FORCES = lambda s: 12.0/s**13 - 6.0/s**7 # Analytic derivative of LJ with minus sign: F = -d/dx Ep
+    """
+    pass
 
 def train_neural_network(x, epochs, nNodes, hiddenLayers,neighbors=5):
     # Begin session
@@ -110,7 +122,7 @@ def train_neural_network(x, epochs, nNodes, hiddenLayers,neighbors=5):
         # Setup of graph for later computation with tensorflow
         prediction, weights, biases, neurons = neuralNetwork(x)
         cost      = tf.nn.l2_loss(tf.sub(prediction, y))
-        optimizer = tf.train.AdamOptimizer(0.005).minimize(cost)
+        optimizer = tf.train.AdamOptimizer().minimize(cost)
 
         # Number of cycles of feed-forward and backpropagation
         numberOfEpochs    = epochs;
@@ -132,7 +144,7 @@ def train_neural_network(x, epochs, nNodes, hiddenLayers,neighbors=5):
         # Loop over epocs
         for epoch in range(0, numberOfEpochs):
             # Generate new data
-            xTrain, yTrain, xTest, yTest = createDataCFDA(trainSize,testSize,neighbors)
+            xTrain, yTrain, xTest, yTest = createData(trainSize,testSize,neighbors) # TODO: Fix
 
             # loop through batches and cover new data set for each epoch
             _, epochLoss = sess.run([optimizer, cost], feed_dict={x: xTrain, y: yTrain})
@@ -199,31 +211,41 @@ def saveGraphFunc(sess, weights, biases, epoch):
                 outFile.write(" ")
             outFile.write("\n")
 
-#--------------#
-##### main #####
-#--------------#
+if __name__ == '__main__':
+    #--------------#
+    ##### main #####
+    #--------------#
+    r_low     = 2
+    r_high    = 5
+    size      = 100
+    neighbors = 30
+    PES   = PES_Stillinger_Weber
+    xyz_N = createXYZ(r_low, r_high, size, neighbors)
+    Ep    = potentialEnergyGenerator(xyz_N, PES)
+    print Ep
 
-tf.reset_default_graph() # Perhaps unneccessary
+    if False:
+        tf.reset_default_graph() # Perhaps unneccessary
 
-# number of samples
-testSize  = 100  # Noise free data
-trainSize = 5000
+        # number of samples
+        testSize  = 100  # Noise free data
+        trainSize = 5000
 
-# number of inputs and outputs
-numberOfNeighbors = 5
-input_vars  = 4*numberOfNeighbors  # Positions x,y,z and r of N neighbor atoms
-output_vars = 4                    # Force sum x,y,z and potential energy
+        # number of inputs and outputs
+        numberOfNeighbors = 5
+        input_vars  = 4*numberOfNeighbors  # Positions x,y,z and r of N neighbor atoms
+        output_vars = 4                    # Force sum x,y,z and potential energy
 
-x = tf.placeholder('float', shape=(None, input_vars),  name="x")
-y = tf.placeholder('float', shape=(None, output_vars), name="y")
+        x = tf.placeholder('float', shape=(None, input_vars),  name="x")
+        y = tf.placeholder('float', shape=(None, output_vars), name="y")
 
-#neuralNetwork = lambda data : nns.modelRelu(data, nNodes=nNodes, hiddenLayers=hiddenLayers, inputs=2, wInitMethod='normal', bInitMethod='normal')
-#neuralNetwork = lambda data : nns.modelTanh(data, nNodes=nNodes, hiddenLayers=hiddenLayers,inputs=2, wInitMethod='normal', bInitMethod='normal')
-neuralNetwork = lambda data : nns.modelSigmoid(data, nNodes=nNodes, hiddenLayers=hiddenLayers,\
-                inputs=input_vars, outputs=output_vars,wInitMethod='normal', bInitMethod='zeros')
+        #neuralNetwork = lambda data : nns.modelRelu(data, nNodes=nNodes, hiddenLayers=hiddenLayers, inputs=2, wInitMethod='normal', bInitMethod='normal')
+        #neuralNetwork = lambda data : nns.modelTanh(data, nNodes=nNodes, hiddenLayers=hiddenLayers,inputs=2, wInitMethod='normal', bInitMethod='normal')
+        neuralNetwork = lambda data : nns.modelSigmoid(data, nNodes=nNodes, hiddenLayers=hiddenLayers,\
+                        inputs=input_vars, outputs=output_vars,wInitMethod='normal', bInitMethod='zeros')
 
-print "---------------------------------------"
-epochs       = 100000
-nNodes       = 50
-hiddenLayers = 2
-weights, biases, neurons, epochlossPerN = train_neural_network(x, epochs, nNodes, hiddenLayers, numberOfNeighbors)
+        print "---------------------------------------"
+        epochs       = 100000
+        nNodes       = 50
+        hiddenLayers = 2
+        weights, biases, neurons, epochlossPerN = train_neural_network(x, epochs, nNodes, hiddenLayers, numberOfNeighbors)
---
+++ symmetry_functions.py
@@ -0,0 +1,185 @@
+from math import cos,pi,tanh,acos,sqrt # Faster than numpy for scalars
+import numpy as np
+
+def cutoff_tanh(r,rc):
+    """
+    Can take scalar and vector input of r and evaluate the cutoff function
+    """
+    rc = float(rc)
+    if type(r) == int:
+        if r <= rc:
+            return tanh(1-r/rc)**3
+        else:
+            return 0.
+    else:
+        return np.tanh(1-r/rc)**3 * (r <= rc)
+
+def cutoff_cos(r,rc):
+    """
+    Can take scalar, vector or matrix input of r and evaluate the cutoff function
+    """
+    # rc = float(rc)
+    if type(r) == int:
+        if r <= rc:
+            return 0.5*(cos(pi*r/rc)+1)
+        else:
+            return 0.
+    else:
+        return 0.5*(np.cos(pi*r/rc)+1) * (r <= rc)
+
+def G1(r, rc, cutoff=cutoff_cos):
+    r_cut     = cutoff(r,rc)
+    summation = np.sum( r_cut )
+    return summation
+
+def G2(r, rc, rs, eta, cutoff=cutoff_cos):
+    r_cut     = cutoff(r,rc)
+    summation = np.sum( np.exp(-eta*(r-rs)**2)*r_cut )
+    return summation
+
+def G3(r, rc, kappa, cutoff=cutoff_cos):
+    r_cut     = cutoff(r,rc)
+    summation = np.sum( np.cos(kappa*r)*r_cut )
+    return summation
+
+def G4(xyz, rc, eta, zeta, lambda_c, cutoff=cutoff_cos):
+    """ xyz:
+    [[x1 y1 z1]
+     [x2 y2 z2]
+     [x3 y3 z3]
+     [x4 y4 z4]]
+     """
+    r         = np.linalg.norm(xyz,axis=1)
+    N         = len(r)
+    r_cut     = cutoff(r,rc)
+    summation = 0
+    for j in range(N):
+        for k in range(N):
+            r_jk       = np.linalg.norm(xyz[j] - xyz[k])
+            theta_ijk  = acos( np.dot(xyz[j],xyz[k]) / (r[j]*r[k]) )
+            cutoff_ijk = r_cut[j]*r_cut[k]*cutoff(r_jk)
+            part_sum   = (1+lambda_c * cos(theta_ijk))**zeta * exp(-eta*(r[j]**2+r[k]**2+r_jk**2))
+            summation += part_sum*cutoff_ijk
+    summation *= 2**(1-zeta)
+    return
+
+def G5(xyz, rc, eta, zeta, lambda_c, cutoff=cutoff_cos):
+    """ xyz:
+    [[x1 y1 z1]
+     [x2 y2 z2]
+     [x3 y3 z3]
+     [x4 y4 z4]]
+     """
+    r         = np.linalg.norm(xyz,axis=1)
+    N         = len(r)
+    r_cut     = cutoff(r,rc)
+    summation = 0
+    for j in range(N):
+        for k in range(N):
+            theta_ijk  = acos( np.dot(xyz[j],xyz[k]) / (r[j]*r[k]) )
+            cutoff_ijk = r_cut[j]*r_cut[k]
+            part_sum   = (1+lambda_c * cos(theta_ijk))**zeta * exp(-eta*(r[j]**2+r[k]**2))
+            summation += part_sum*cutoff_ijk
+    summation *= 2**(1-zeta)
+    return
+
+"""
+#################
+Next functions are mainly for testing purposes
+i.e. plotting response-curves etc.
+#################
+"""
+
+def G1_single_neighbor(r, rc, cutoff=cutoff_cos):
+    return cutoff(r,rc)
+
+def G2_single_neighbor(r, rc, rs, eta, cutoff=cutoff_cos):
+    r_cut     = cutoff(r,rc)
+    return np.exp(-eta*(r-rs)**2)*r_cut
+
+def G3_single_neighbor(r, rc, kappa, cutoff=cutoff_cos):
+    r_cut     = cutoff(r,rc)
+    return np.cos(kappa*r)*r_cut
+
+def G4_single_neighbor_rjk(theta, rc, zeta, lambda_c, eta, cutoff=cutoff_cos, percent_of_rc=0.8):
+    """
+    rij   = rik = 0.8 Rc
+    theta = 0,..,360
+    """
+    rij           = percent_of_rc * rc
+    cos_theta     = np.cos(theta)
+    rjk           = sqrt(2) * rij * np.sqrt(1 - cos_theta) # Simplified law of cosines
+    exp_factor    = np.exp(-eta*(2*rij**2 + rjk**2))
+    angle_factor  = 2**(1-zeta) * (1 + lambda_c * cos_theta)**zeta
+    cutoff_factor = cutoff(rij, rc)**2 * cutoff(rjk, rc)
+    return angle_factor * exp_factor * cutoff_factor
+
+def G4_single_neighbor_radial(r, zeta, lambda_c, eta):
+    """
+    adfds
+    """
+    theta = pi/3. # Constant at 60 degrees aka pi/3
+    exp_factor   = np.exp(-eta*3*r**2)
+    angle_factor = 2**(1-zeta) * (1 + lambda_c * np.cos(theta))**zeta
+    return angle_factor * exp_factor
+
+def G4_single_neighbor_2D(theta_grid, rc_grid, r_all, zeta, lambda_c, eta):
+    cutoff        = cutoff_cos
+    rij           = r_all # rij = rik
+    cos_theta     = np.cos(theta_grid)
+    rjk           = sqrt(2) * rij * np.sqrt(1 - cos_theta) # Simplified law of cosines
+    exp_factor    = np.exp(-eta*(2*rij**2 + rjk**2))
+    angle_factor  = 2**(1-zeta) * (1 + lambda_c * cos_theta)**zeta
+    cutoff_factor = cutoff(rij, rc_grid)**2 * cutoff(rjk, rc_grid)
+    return angle_factor * exp_factor * cutoff_factor
+
+def G4_single_neighbor(theta, r_all, rc, zeta, lambda_c, eta):
+    """
+    NB: Number 444444444444444444444, not 5
+    """
+    cutoff        = cutoff_cos
+    rij           = r_all # rij = rik
+    cos_theta     = np.cos(theta)
+    rjk           = sqrt(2) * rij * np.sqrt(1 - cos_theta) # Simplified law of cosines
+    exp_factor    = np.exp(-eta*(2*rij**2 + rjk**2))
+    angle_factor  = 2**(1-zeta) * (1 + lambda_c * cos_theta)**zeta
+    cutoff_factor = cutoff(rij, rc)**2 * cutoff(rjk, rc)
+    return angle_factor * exp_factor * cutoff_factor
+
+def G5_single_neighbor(theta, r_all, rc, zeta, lambda_c, eta):
+    """
+    Assumes cutoffs to be normalized to 1 and is removed from eqs
+    """
+    cutoff        = cutoff_cos
+    rij           =  r_all # Both equal
+    exp_factor    = np.exp(-eta*2*rij**2)
+    angle_factor  = 2**(1-zeta) * (1 + lambda_c * np.cos(theta))**zeta
+    cutoff_factor = cutoff(rij, rc)**2
+    return angle_factor * exp_factor * cutoff_factor
+
+def G5_single_neighbor_radial(r, zeta, lambda_c, eta):
+    """
+    Radial part of G5 when rij = rik
+    """
+    theta = pi/3. # Constant at 60 degrees aka pi/3
+    exp_factor   = np.exp(-eta*2*r**2) # rij = rik
+    angle_factor = 2**(1-zeta) * (1 + lambda_c * np.cos(theta))**zeta
+    return angle_factor * exp_factor
+
+def G5_single_neighbor_rjk(theta, rc, zeta, lambda_c, eta, cutoff=cutoff_cos, percent_of_rc=0.8):
+    """
+    rij   = rik = 0.8 Rc
+    theta = 0,..,360
+    """
+    rij           = percent_of_rc * rc
+    cos_theta     = np.cos(theta)
+    exp_factor    = np.exp(-eta*2*rij**2)
+    angle_factor  = 2**(1-zeta) * (1 + lambda_c * cos_theta)**zeta
+    cutoff_factor = cutoff(rij, rc)**2
+    return angle_factor * exp_factor * cutoff_factor
+
+if __name__ == '__main__':
+    """
+    Mainly for testing purpose
+    """
+    print "This does absolutely nothing, I'm afraid dear!"
