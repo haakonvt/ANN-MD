3a7a459a0ad10e9ff0405c76811ad7ee3d490204
Håkon Treider <haakonvt@gmail.com>
2017-02-07 14:43:52 +0100

    Stillinger Weber works

--- create_train_data.py
+++ create_train_data.py
@@ -1,18 +1,14 @@
 """
-Train a neural network to approximate a potential energy surface
-with the use of symmetry functions that transform xyz-data.
+Generate XYZ data with corresponding energies from some chosen PES:
+- Lennard Jones
+- Stillinger Weber etc.
 """
-# from symmetry_functions import G1, G2, G3, G4, G5
-from symmetry_transform import symmetryTransform
-from mpl_toolkits.mplot3d import Axes3D
 from math import pi,sqrt,acos,exp,cos
-import neural_network_setup as nns
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 import tensorflow as tf
 import numpy as np
-import sys,os
-import glob
+import sys
+
+np.random.seed(1) # For testing
 
 def potentialEnergyGenerator(xyz_N, PES):
     size = xyz_N.shape[2]
@@ -81,15 +77,14 @@ def PES_Stillinger_Weber(xyz_i):
 
     rc = (r < a) # Bool array. "False" cast to 0 and "True" to 1
     U2 = eps*A*(B*r**(-p)-r**(-q)) * np.exp(r-a)**-1 * rc
-    U3 = lambda rij, rik, theta_ijk: eps*l*exp(g/(rij-a) + g/(rik-a)) \
-                                     * (cos(theta_ijk) - cos_tc)**2   \
-                                     * (rij < a) * (rik < a)
-
-    U = np.sum(U2) # Two body terms
+    U  = np.sum(U2) # Two body terms
+    U3 = lambda rij, rik, theta_ijk: eps*l*exp(g/(rij-a) + g/(rik-a))* (cos(theta_ijk) - cos_tc)**2 \
+                                     if (rij < a) and (rik < a) else 0.0 # Is this bad practice? lawls
+    # Need a double sum to find three body terms
     for j in range(N): # i < j
-        for k in range(j,N): # i < j < k
-            theta_ijk  = acos(np.dot(xyz[j],xyz[k]) / (r[j]*r[k])) # TODO: Angle sometimes wrong (abs > 1)
-            U         += U3(r[j], r[k], theta_ijk) # Three body terms
+        for k in range(j+1,N): # i < j < k
+            theta = acos(np.dot(xyz[j],xyz[k]) / (r[j]*r[k]))
+            U    += U3(r[j], r[k], theta)
     return U
 
 def FORCES_Stillinger_Weber(xyz_i):
@@ -116,136 +111,16 @@ def FORCES_Lennard_Jones(xyz_i):
     """
     pass
 
-def train_neural_network(x, epochs, nNodes, hiddenLayers,neighbors=5):
-    # Begin session
-    with tf.Session() as sess:
-        # Setup of graph for later computation with tensorflow
-        prediction, weights, biases, neurons = neuralNetwork(x)
-        cost      = tf.nn.l2_loss(tf.sub(prediction, y))
-        optimizer = tf.train.AdamOptimizer().minimize(cost)
-
-        # Number of cycles of feed-forward and backpropagation
-        numberOfEpochs    = epochs;
-        bestEpochTestLoss = -1
-        startEpoch        = 0
-
-        # Initialize variables or restore from file
-        saver = tf.train.Saver(weights + biases, max_to_keep=None)
-        sess.run(tf.initialize_all_variables())
-
-        # Save first version of the net
-        saveFileName = "SavedRuns/run" + str(0) + ".dat"
-        saver.save(sess, saveFileName, write_meta_graph=False)
-        saveGraphFunc(sess, weights, biases, 0)
-
-        bestTrainLoss = 1E100; bestTestLoss = 1E100; bestTestLossSinceLastPlot = 1E100; prevErr = 1E100
-        triggerNewLine = False; saveNow = False
-
-        # Loop over epocs
-        for epoch in range(0, numberOfEpochs):
-            # Generate new data
-            xTrain, yTrain, xTest, yTest = createData(trainSize,testSize,neighbors) # TODO: Fix
-
-            # loop through batches and cover new data set for each epoch
-            _, epochLoss = sess.run([optimizer, cost], feed_dict={x: xTrain, y: yTrain})
-
-            # compute test set loss
-            # THIS IS MAYBE USED TO TRAIN?? Gotta check!!
-            #_, testCost = sess.run(prediction, feed_dict={x: xTest})
-            _, testCost = sess.run([optimizer, cost], feed_dict={x: xTest, y: yTest})
-
-            if epoch%800 == 0:
-                triggerNewLine = True
-            if epoch%80  == 0 and numberOfEpochs > epoch+80:
-                sys.stdout.write('\r' + ' '*80) # White out line
-                sys.stdout.write('\rEpoch %5d out of %5d trainloss/N: %10g, testloss/N: %10g' % \
-                      (epoch+1, numberOfEpochs, epochLoss/float(trainSize), testCost/float(testSize)))
-                sys.stdout.flush()
-            if testCost < bestTestLoss:
-                bestTestLoss = testCost
-                bestEpochTestLoss = epoch
-                if triggerNewLine:
-                    sys.stdout.write('\r' + ' '*80) # White out line
-                    sys.stdout.write('\rEpoch %5d out of %5d trainloss/N: %10g, testloss/N: %10g\n' % \
-                          (epoch+1, numberOfEpochs, epochLoss/float(trainSize), testCost/float(testSize)))
-                    sys.stdout.flush()
-                    # If saving is enabled, save the graph variables ('w', 'b')
-                    if True:#saveFlag:
-                        saveFileName = "SavedRuns/run" + str(epoch+1) + ".dat"
-                        saver.save(sess, saveFileName, write_meta_graph=False)
-                        saveGraphFunc(sess, weights, biases, epoch+1)
-                triggerNewLine = False
-
-    sys.stdout.write('\r' + ' '*80) # White out line for sake of pretty command line output lol
-    return weights, biases, neurons, bestTestLoss/float(testSize)
-
-def saveGraphFunc(sess, weights, biases, epoch):
-    try:
-        os.mkdir("SavedGraphs")
-    except:
-        pass
-    saveGraphName = "SavedGraphs/tf_graph_WB_%d.txt" %(epoch)
-    with open(saveGraphName, 'w') as outFile:
-        outStr = "%1d %1d %s" % (hiddenLayers, nNodes, "sigmoid")
-        outFile.write(outStr + '\n')
-        size = len(sess.run(weights))
-        for i in range(size):
-            i_weights = sess.run(weights[i])
-            if i < size-1:
-                for j in range(len(i_weights)):
-                    for k in range(len(i_weights[0])):
-                        outFile.write("%g" % i_weights[j][k])
-                        outFile.write(" ")
-                    outFile.write("\n")
-            else:
-                for j in range(len(i_weights[0])):
-                    for k in range(len(i_weights)):
-                        outFile.write("%g" % i_weights[k][j])
-                        outFile.write(" ")
-                    outFile.write("\n")
-        outFile.write("\n")
-        for biasVariable in biases:
-            i_biases = sess.run(biasVariable)
-            for j in range(len(i_biases)):
-                outFile.write("%g" % i_biases[j])
-                outFile.write(" ")
-            outFile.write("\n")
 
 if __name__ == '__main__':
-    #--------------#
-    ##### main #####
-    #--------------#
-    r_low     = 2
-    r_high    = 5
-    size      = 100
-    neighbors = 30
-    PES   = PES_Stillinger_Weber
-    xyz_N = createXYZ(r_low, r_high, size, neighbors)
-    Ep    = potentialEnergyGenerator(xyz_N, PES)
-    print Ep
 
-    if False:
-        tf.reset_default_graph() # Perhaps unneccessary
-
-        # number of samples
-        testSize  = 100  # Noise free data
-        trainSize = 5000
-
-        # number of inputs and outputs
-        numberOfNeighbors = 5
-        input_vars  = 4*numberOfNeighbors  # Positions x,y,z and r of N neighbor atoms
-        output_vars = 4                    # Force sum x,y,z and potential energy
-
-        x = tf.placeholder('float', shape=(None, input_vars),  name="x")
-        y = tf.placeholder('float', shape=(None, output_vars), name="y")
-
-        #neuralNetwork = lambda data : nns.modelRelu(data, nNodes=nNodes, hiddenLayers=hiddenLayers, inputs=2, wInitMethod='normal', bInitMethod='normal')
-        #neuralNetwork = lambda data : nns.modelTanh(data, nNodes=nNodes, hiddenLayers=hiddenLayers,inputs=2, wInitMethod='normal', bInitMethod='normal')
-        neuralNetwork = lambda data : nns.modelSigmoid(data, nNodes=nNodes, hiddenLayers=hiddenLayers,\
-                        inputs=input_vars, outputs=output_vars,wInitMethod='normal', bInitMethod='zeros')
-
-        print "---------------------------------------"
-        epochs       = 100000
-        nNodes       = 50
-        hiddenLayers = 2
-        weights, biases, neurons, epochlossPerN = train_neural_network(x, epochs, nNodes, hiddenLayers, numberOfNeighbors)
+    if True:
+        # Stillinger Weber test
+        r_low     = 0.8
+        r_high    = 1.9
+        size      = 100
+        neighbors = 30
+        PES       = PES_Stillinger_Weber
+        xyz_N     = createXYZ(r_low, r_high, size, neighbors)
+        Ep        = potentialEnergyGenerator(xyz_N, PES)
+        print Ep
---
+++ training_nn.py
@@ -0,0 +1,132 @@
+"""
+Train a neural network to approximate a potential energy surface
+with the use of symmetry functions that transform xyz-data.
+"""
+import neural_network_setup as nns
+import tensorflow as tf
+import numpy as np
+import sys,os
+import glob
+
+def train_neural_network(x, epochs, nNodes, hiddenLayers,neighbors=5):
+    # Begin session
+    with tf.Session() as sess:
+        # Setup of graph for later computation with tensorflow
+        prediction, weights, biases, neurons = neuralNetwork(x)
+        cost      = tf.nn.l2_loss(tf.sub(prediction, y))
+        optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+        # Number of cycles of feed-forward and backpropagation
+        numberOfEpochs    = epochs;
+        bestEpochTestLoss = -1
+        startEpoch        = 0
+
+        # Initialize variables or restore from file
+        saver = tf.train.Saver(weights + biases, max_to_keep=None)
+        sess.run(tf.initialize_all_variables())
+
+        # Save first version of the net
+        saveFileName = "SavedRuns/run" + str(0) + ".dat"
+        saver.save(sess, saveFileName, write_meta_graph=False)
+        saveGraphFunc(sess, weights, biases, 0)
+
+        bestTrainLoss = 1E100; bestTestLoss = 1E100; bestTestLossSinceLastPlot = 1E100; prevErr = 1E100
+        triggerNewLine = False; saveNow = False
+
+        # Loop over epocs
+        for epoch in range(0, numberOfEpochs):
+            # Generate new data
+            xTrain, yTrain, xTest, yTest = createData(trainSize,testSize,neighbors) # TODO: Fix
+
+            # loop through batches and cover new data set for each epoch
+            _, epochLoss = sess.run([optimizer, cost], feed_dict={x: xTrain, y: yTrain})
+
+            # compute test set loss
+            # THIS IS MAYBE USED TO TRAIN?? Gotta check!!
+            #_, testCost = sess.run(prediction, feed_dict={x: xTest})
+            _, testCost = sess.run([optimizer, cost], feed_dict={x: xTest, y: yTest})
+
+            if epoch%800 == 0:
+                triggerNewLine = True
+            if epoch%80  == 0 and numberOfEpochs > epoch+80:
+                sys.stdout.write('\r' + ' '*80) # White out line
+                sys.stdout.write('\rEpoch %5d out of %5d trainloss/N: %10g, testloss/N: %10g' % \
+                      (epoch+1, numberOfEpochs, epochLoss/float(trainSize), testCost/float(testSize)))
+                sys.stdout.flush()
+            if testCost < bestTestLoss:
+                bestTestLoss = testCost
+                bestEpochTestLoss = epoch
+                if triggerNewLine:
+                    sys.stdout.write('\r' + ' '*80) # White out line
+                    sys.stdout.write('\rEpoch %5d out of %5d trainloss/N: %10g, testloss/N: %10g\n' % \
+                          (epoch+1, numberOfEpochs, epochLoss/float(trainSize), testCost/float(testSize)))
+                    sys.stdout.flush()
+                    # If saving is enabled, save the graph variables ('w', 'b')
+                    if True:#saveFlag:
+                        saveFileName = "SavedRuns/run" + str(epoch+1) + ".dat"
+                        saver.save(sess, saveFileName, write_meta_graph=False)
+                        saveGraphFunc(sess, weights, biases, epoch+1)
+                triggerNewLine = False
+
+    sys.stdout.write('\r' + ' '*80) # White out line for sake of pretty command line output lol
+    return weights, biases, neurons, bestTestLoss/float(testSize)
+
+def saveGraphFunc(sess, weights, biases, epoch):
+    try:
+        os.mkdir("SavedGraphs")
+    except:
+        pass
+    saveGraphName = "SavedGraphs/tf_graph_WB_%d.txt" %(epoch)
+    with open(saveGraphName, 'w') as outFile:
+        outStr = "%1d %1d %s" % (hiddenLayers, nNodes, "sigmoid")
+        outFile.write(outStr + '\n')
+        size = len(sess.run(weights))
+        for i in range(size):
+            i_weights = sess.run(weights[i])
+            if i < size-1:
+                for j in range(len(i_weights)):
+                    for k in range(len(i_weights[0])):
+                        outFile.write("%g" % i_weights[j][k])
+                        outFile.write(" ")
+                    outFile.write("\n")
+            else:
+                for j in range(len(i_weights[0])):
+                    for k in range(len(i_weights)):
+                        outFile.write("%g" % i_weights[k][j])
+                        outFile.write(" ")
+                    outFile.write("\n")
+        outFile.write("\n")
+        for biasVariable in biases:
+            i_biases = sess.run(biasVariable)
+            for j in range(len(i_biases)):
+                outFile.write("%g" % i_biases[j])
+                outFile.write(" ")
+            outFile.write("\n")
+
+if __name__ == '__main__':
+
+    if False:
+        tf.reset_default_graph() # Perhaps unneccessary
+
+        # number of samples
+        testSize  = 100  # Noise free data
+        trainSize = 5000
+
+        # number of inputs and outputs
+        numberOfNeighbors = 5
+        input_vars  = 4*numberOfNeighbors  # Positions x,y,z and r of N neighbor atoms
+        output_vars = 4                    # Force sum x,y,z and potential energy
+
+        x = tf.placeholder('float', shape=(None, input_vars),  name="x")
+        y = tf.placeholder('float', shape=(None, output_vars), name="y")
+
+        #neuralNetwork = lambda data : nns.modelRelu(data, nNodes=nNodes, hiddenLayers=hiddenLayers, inputs=2, wInitMethod='normal', bInitMethod='normal')
+        #neuralNetwork = lambda data : nns.modelTanh(data, nNodes=nNodes, hiddenLayers=hiddenLayers,inputs=2, wInitMethod='normal', bInitMethod='normal')
+        neuralNetwork = lambda data : nns.modelSigmoid(data, nNodes=nNodes, hiddenLayers=hiddenLayers,\
+                        inputs=input_vars, outputs=output_vars,wInitMethod='normal', bInitMethod='zeros')
+
+        print "---------------------------------------"
+        epochs       = 100000
+        nNodes       = 50
+        hiddenLayers = 2
+        weights, biases, neurons, epochlossPerN = train_neural_network(x, epochs, nNodes, hiddenLayers, numberOfNeighbors)
