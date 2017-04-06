0763825210b15ddb81164bc2f2f76032ba39d688
Håkon Treider <haakonvt@gmail.com>
2017-04-06 20:48:04 +0200

    NN vs SW Ep check finished

--- create_train_data.py
+++ create_train_data.py
@@ -125,16 +125,31 @@ def PES_Stillinger_Weber(xyz_i):
         else:
             return 0.0
     # Sum up two body terms
-    U  = np.sum(U2)
-    if isnan(U):
-        U = U2_serial(r, rc)
-        print "\nNaN gotten, re-computing with serial code. U2 = ", U
+    U2_sum  = np.sum(U2)/2 # Each pair share this energy. Contribute half to each
+    if isnan(U2_sum):
+        U2_sum = U2_serial(r, rc)
+        print "\nNaN gotten, re-computing with serial code. U2 = ", U2_sum
     # Need a double sum to find three body terms
+    U3_sum = 0.0
     for j in range(N): # i < j
         for k in range(j+1,N): # i < j < k
-            cos_theta = np.dot(xyz[j],xyz[k]) / (r[j]*r[k])
-            U        += U3(r[j], r[k], cos_theta)
-    return U
+            cos_theta = np.dot(xyz[j], xyz[k]) / (r[j]*r[k])
+            U3_sum   += U3(r[j], r[k], cos_theta)
+    """
+    # Total system energy:
+    xyz = np.insert(xyz,0,np.zeros(3),axis=0) # Add our 0,0,0 atom
+    for i in range(N+1):
+        for j in range(i+1,N+1): # i < j
+            for k in range(j+1,N+1): # i < j < k
+                xyz_j = xyz[j]-xyz[i]
+                xyz_k = xyz[k]-xyz[i]
+                rij   = np.linalg.norm(xyz_j)
+                rik   = np.linalg.norm(xyz_k)
+                cos_theta = np.dot(xyz_j, xyz_k) / (rij*rik)
+                U3_sum   += U3(rij, rik, cos_theta)
+    """
+    U3_sum /= 3 # Each triplet share this energy. Contribute a third to each
+    return U3_sum + U2_sum
 
 def createTrainData(size, neighbors, PES, verbose=False):
     if PES == PES_Stillinger_Weber:
@@ -468,7 +483,7 @@ def NeighListDataToSymmToFile(open_filename, save_filename, size):
                 percent = round(float(i+1)/size*100., 2)
                 sys.stdout.write('\rTransforming xyz with symmetry functions. %.2f %% complete' %(percent))
                 sys.stdout.flush()
-    print "\nNmbr of lines in file", i+1, ", length Ep:", len(Ep), ", size --> file:", size, 
+    print "\nNmbr of lines in file", i+1, ", length Ep:", len(Ep), ", size --> file:", size
     dump_data = np.zeros((size, nmbr_G + 1))
     dump_data[:,0]  = Ep
     dump_data[:,1:] = xTrain
@@ -540,15 +555,13 @@ if __name__ == '__main__':
     """
     Suggestion: Only have ONE option set to True at the time.
     """
-    # Based on random structures, fixed number of neighbors
+    # Based on random structures, fixed/variable number of neighbors
     dumpToFile         = False
-    concatenateFiles   = False # This can be set to True though if
-    # As above, but variable number of neighbors
-    dumpMultiple       = False
+    dumpMultiple       = True
 
     # Structures from SW run in lammps
-    xyz_to_neigh_lists = True
-    dumpXYZ_file       = True # Own algo: "readXYZ_Files"
+    xyz_to_neigh_lists = False
+    dumpXYZ_file       = False # Own algo: "readXYZ_Files"
 
     # Neig.lists from lammps with John-Anders algo
     dumpLammpsFile     = False
@@ -593,7 +606,7 @@ if __name__ == '__main__':
         if True:
             # This is SW
             size = 5000
-            neighbors = 7
+            neighbors = 2
             # filename = "stillinger-weber-symmetry-data.txt"
             filename  = "SW_train_rs_%s_n%s.txt" %(str(size), str(neighbors))
             print "When run directly (like now), this file dumps training data to file:"
@@ -601,7 +614,7 @@ if __name__ == '__main__':
             print "-------------------------------"
             print "Neighbors", neighbors
             print "-------------------------------"
-            PES       = PES_Stillinger_Weber
+            PES = PES_Stillinger_Weber
             t0 = timer()
             createTrainDataDump(size, neighbors, PES, filename, \
                                 only_concatenate=concatenateFiles, verbose=True)
@@ -609,7 +622,7 @@ if __name__ == '__main__':
             print "\nComputation took: %.2f seconds" %t1
         else:
             # This is LJ
-            size = 100000
+            size = 10000
             neighbors = 8
             # filename = "stillinger-weber-symmetry-data.txt"
             filename  = "LJ_train_rs_%s_n%s.txt" %(str(size), str(neighbors))
@@ -621,13 +634,14 @@ if __name__ == '__main__':
             PES       = PES_Lennard_Jones
             t0 = timer()
             createTrainDataDump(size, neighbors, PES, filename, \
-                                only_concatenate=concatenateFiles, verbose=True)
+                                only_concatenate=False, verbose=True)
             t1 = timer() - t0
             print "\nComputation took: %.2f seconds" %t1
 
     if dumpMultiple:
-        size = 25 # PER single value in neigh_list
-        neigh_list = [4]*2 + [5]*6 + [6]*13 + [7]*14 + [8]*9 + [9]*3 + [10] # len: 48
+        size = 2500 # PER single value in neigh_list
+        # neigh_list = [4]*2 + [5]*6 + [6]*13 + [7]*14 + [8]*9 + [9]*3 + [10] # len: 48
+        neigh_list = [1, 2]
         t0_tot = timer()
         PES = PES_Stillinger_Weber
         for ID,neighbors in enumerate(neigh_list):
@@ -639,13 +653,13 @@ if __name__ == '__main__':
             print "-------------------------------"
             t0  = timer()
             createTrainDataDump(size, neighbors, PES, filename, \
-                                only_concatenate=concatenateFiles, verbose=True, \
+                                only_concatenate=False, verbose=True, \
                                 no_load=True)
             t1 = timer() - t0
             print "\nComputation of %d neighbors took: %.2f seconds" %(neighbors,t1)
         t1 = timer() - t0_tot
         # Load all files into one master file
-        createTrainDataDump(0, 0, PES, "SW_train_manyneigh_final.txt", \
+        createTrainDataDump(0, 0, PES, "SW_train_manyneigh_%d.txt" %(size*len(neigh_list)), \
                             only_concatenate=True, verbose=True, no_load=False)
         if t1 > 1000:
             t1 /= 60.
--- force_verify.py
+++ force_verify.py
@@ -11,18 +11,24 @@ def test_structure_3atom():
            [x2,y2,z2],
            [x3,y3,z3]]
     """
-    h = sqrt(3)
     xyz = np.array([[0,0,0],
-                    [2,0,0],
-                    [1,h,0]], dtype=float)
+                    [2.5,0,0],
+                    [1.25,2.5,0]])
+    xyz = np.array([[9.92203, 11.8905, 11.3479],
+                    [10.9631, 11.0302, 9.46745],
+                    [12.6649, 9.57931, 9.1846]])
+    xyz -= xyz[0,:] # Move particles to 0,0,0 for atom 0
 
-    f_vec_nn = evaluate_NN_forces(xyz)
-    # print "Tot NN force:    ",f_vec_nn
-    f_vec_sw = evaluate_SW_forces(xyz)
-    # print "Tot SW force:    ",f_vec_sw
+    print xyz, np.linalg.norm(xyz[1:,:], axis=1)
+
+    EpFlag = True
+    FFlag  = False
+
+    evaluate_NN_forces(xyz, EpFlag, FFlag)
+    evaluate_SW_forces(xyz, EpFlag, FFlag)
 
 
-def evaluate_SW_forces(xyz):
+def evaluate_SW_forces(xyz, EpFlag, FFlag):
     """
     Stillinger and Weber,  Phys. Rev. B, v. 31, p. 5262, (1985)
 
@@ -44,7 +50,6 @@ def evaluate_SW_forces(xyz):
 
     r  = np.linalg.norm(xyz[1:,:], axis=1) # Only j != i
     U2 = A*eps*(B*(sig/r)**p-(sig/r)**q) * np.exp(sig/(r-a*sig)) * (r < a*sig)
-    U2_sum = np.sum(U2)
 
     dU2dR = -A*eps*sig*(B*(sig/r)**p - (sig/r)**q)*np.exp(sig/(-a*sig + r))/(-a*sig + r)**2 + A*eps*(-B*p*(sig/r)**p/r + q*(sig/r)**q/r)*np.exp(sig/(-a*sig + r))
     for j in [1,2]:
@@ -63,20 +68,23 @@ def evaluate_SW_forces(xyz):
     cos_theta_ikj = np.dot(xyz[0]-xyz[2], xyz[1]-xyz[2]) / (rik*rjk)
     # print "cos Theta:",cos_theta_jik, cos_theta_ijk, cos_theta_ikj
 
-    def h(r1, r2, cos_theta):
-        term = exp(gam*sig/(r1-a*sig)) * exp(gam*sig/(r2-a*sig)) * lamb*eps*(cos_theta - cos0)**2
-        return term
-
-    h_jik = h(rij, rik, cos_theta_jik)
-    h_ijk = h(rij, rjk, cos_theta_ijk)
-    h_ikj = h(rik, rjk, cos_theta_ikj)
-    U3 = h_jik + h_ijk + h_ikj
-
     xyz /= sig
     rij /= sig
     rik /= sig
     rjk /= sig
     xyz_jk = xyz[2]-xyz[1]
+
+    def h(r1, r2, cos_theta):
+        term = eps * lamb * exp(gam/(r1-a)) * exp(gam/(r2-a)) * (cos_theta - cos0)**2
+        term *= (r1 < a) * (r2 < a)
+        return term
+
+    h_jik = h(rij, rik, cos_theta_jik)
+    h_ijk = h(rij, rjk, cos_theta_ijk)
+    h_ikj = h(rik, rjk, cos_theta_ikj)
+
+    # U3 = h_jik + h_ijk + h_ikj
+
     dhjik_dri = -gam*h_jik*((xyz[1]/rij)*1/(rij-a)**2 + (xyz[2]/rik)*1/(rik-a)**2) \
                 + 2*lamb*np.exp(gam/(rij-a) + gam/(rik-a))*(cos_theta_jik - cos0)  \
                 * ((xyz[1]/rij)*(1/rik) + (xyz[2]/rik)*(1/rij) - (xyz[1]/(rij*rik)+xyz[2]/(rik*rij))*cos_theta_jik)
@@ -88,14 +96,23 @@ def evaluate_SW_forces(xyz):
     dhikj_dri = -gam*h_ikj*((xyz[2]/rik)*1/(rik-a)**2) \
                 + 2*lamb*np.exp(gam/(rik-a) + gam/(rjk-a))*(cos_theta_ikj - cos0) \
                 *((-xyz_jk/rjk)*(1/rik) + (xyz[2]/rik)*(1/rik)*cos_theta_ikj)
-    # print "Tot 2-body force:", f_vec
-    # print "Tot 3-body force:", dhjik_dri + dhijk_dri + dhikj_dri
-    print "U_SW:", U3+U2_sum
-    f_vec += dhjik_dri + dhijk_dri + dhikj_dri
-    return f_vec
+    if FFlag:
+        print "Tot 2-body force:", f_vec
+        print "Tot 3-body force:", dhjik_dri + dhijk_dri + dhikj_dri
+        print "Total force     :", f_vec + dhjik_dri + dhijk_dri + dhikj_dri
+
+    if EpFlag:
+        from create_train_data import PES_Stillinger_Weber
+        U2_sum = np.sum(U2)/2.
+        U3_sum = h_jik/3.
+        U_SW   = PES_Stillinger_Weber(xyz[1:,:]*sig)
+        print "Potential E (SW):", U_SW
+        if abs(U3_sum + U2_sum) - abs(U_SW) > 1E-10:
+            print "Pot. E (SW v2)  :", U3_sum + U2_sum
 
 
-def evaluate_NN_forces(xyz):
+
+def evaluate_NN_forces(xyz, EpFlag, FFlag):
     """
     Evaluation checked and fund bug free
     """
@@ -106,16 +123,16 @@ def evaluate_NN_forces(xyz):
     act_function   = lambda x: 1.0/(1+np.exp(-x))
     vec_prev_layer = symm_vec # First input
     node_w_list[0] = node_w_list[0]
+    # Evaluate the neural network:
     for i,w_mat in enumerate(node_w_list):
         out_layer  = np.dot(vec_prev_layer, w_mat)
-        # print i,"A", out_layer
         out_layer += node_biases[i]
-        # print i,"B", out_layer
         if i != len(node_w_list)-1: # We dont use act_func on output layer
             vec_prev_layer = act_function(out_layer)
-            # print i,"C", vec_prev_layer
-    print "U_NN:", float(out_layer)
-    return 0,0,0
+    if EpFlag:
+        print "Potential E (NN):", float(out_layer)
+    if FFlag:
+        print "Forces (NN):", "N/A"
 
 
 def read_NN_from_file():
