Num. derivatives confirms NN gives correct forces YEY

create_train_data.py
@@ -16,6 +16,82 @@ import time
 import sys
 import os
 
+def PES_Lennard_Jones(xyz_i):
+    """
+    Simple LJ pair potential
+    """
+    eps = 1. # 1.0318 * 10^(-2) eV
+    sig = 1. # 3.405 * 10^(-7) meter
+    r   = np.linalg.norm(xyz_i, axis=1)
+    rc  = 1.6*sig
+    LJ0 = abs(4*eps*((sig/rc)**12 - (sig/rc)**6)) # Potential goes to zero at cut
+    LJ  = 4*eps*((sig/r)**12 - (sig/r)**6) * (r < rc) + LJ0
+    U   =  np.sum( LJ )
+    return U
+
+def PES_Stillinger_Weber(xyz_i):
+    """
+    INPUT
+    - xyz_i: Matrix with columnds containing cartesian coordinates,
+           relative to the current atom i, i.e.:
+           [[x1 y1 z1]
+            [x2 y2 z2]
+            [x3 y3 z3]
+            [x4 y4 z4]]
+    """
+    xyz = xyz_i
+    r = np.linalg.norm(xyz, axis=1)
+    N = len(r) # Number of neighbors for atom i, which we are currently inspecting
+
+    # A lot of definitions first
+    A = 7.049556277
+    B = 0.6022245584
+    p = 4.
+    q = 0.
+    a = 1.8
+    l = 21. # lambda
+    g = 1.2 # gamma
+    cos_tc = -1.0/3.0 # 109.47 deg
+
+    eps = 2.1683 # [eV]
+    sig = 2.0951 # [Å]
+
+    rc = (r < a*sig) # Bool array. "False" cast to 0 and "True" to 1
+    # filterwarnings("ignore", category=RuntimeWarning) # U2 below can give NaN
+    U2 = A*eps*(B*(sig/r)**p-(sig/r)**q) * np.exp(sig/(r-a*sig)) * rc
+    # filterwarnings("always", category=RuntimeWarning) # Turn warnings back on
+    def U2_serial(r_vec, r_cut): # Slow, i.e. only use if U2 gives NaN
+        U2_E = 0
+        for r,rc in zip(r_vec,r_cut):
+            if rc:
+                U2_E += A*eps*(B*(sig/r)**p-(sig/r)**q) * np.exp(sig/(r-a*sig))
+            else:
+                pass # Add 0
+        return U2_E
+    def U3(rij, rik, cos_theta):
+        if (rij < a*sig) and (rik < a*sig):
+            exp_factor   = exp(g*sig/(rij-a*sig)) * exp(g*sig/(rik-a*sig))
+            angle_factor = l*eps*(cos_theta - cos_tc)**2
+            return exp_factor * angle_factor
+        else:
+            return 0.0
+    # Sum up two body terms
+    U2_sum  = np.sum(U2)
+    if isnan(U2_sum): # NaN gotten, re-computing with serial code. U2 = ", U2_sum
+        U2_sum = U2_serial(r, rc)
+    # Need a double sum to find three body terms
+    U3_sum = 0.0
+    for j in range(N): # j != i
+        v_rij = xyz[j] # Only depend on j
+        rij   = r[j]
+        for k in range(j+1,N): # i < j < k
+            v_rik = xyz[k]
+            rik   = r[k]
+            cos_theta_jik = np.dot(v_rij,  v_rik) / (rij*rik)
+            U3_sum += U3(rij, rik, cos_theta_jik)
+    U_total = U2_sum/2.0 + U3_sum
+    return U_total
+
 def potentialEnergyGenerator(xyz_N, PES):
     if len(xyz_N.shape) == 2: # This is just a single neighbor list
         return PES(xyz_N)
@@ -63,82 +139,6 @@ def createXYZ(r_min, r_max, size, neighbors=7, histogramPlot=False, verbose=Fals
         import matplotlib.pyplot as plt
         plt.subplot(3,1,1);plt.hist(xyz_N[:,0,:].ravel(),bins=70);plt.subplot(3,1,2);plt.hist(xyz_N[:,1,:].ravel(),bins=70);plt.subplot(3,1,3);plt.hist(xyz_N[:,2,:].ravel(),bins=70);plt.show()
     return xyz_N
-
-def PES_Lennard_Jones(xyz_i):
-    """
-    Simple LJ pair potential
-    """
-    eps = 1. # 1.0318 * 10^(-2) eV
-    sig = 1. # 3.405 * 10^(-7) meter
-    r   = np.linalg.norm(xyz_i, axis=1)
-    rc  = 1.6*sig
-    LJ0 = abs(4*eps*((sig/rc)**12 - (sig/rc)**6)) # Potential goes to zero at cut
-    LJ  = 4*eps*((sig/r)**12 - (sig/r)**6) * (r < rc) + LJ0
-    U   =  np.sum( LJ )
-    return U
-
-def PES_Stillinger_Weber(xyz_i):
-    """
-    INPUT
-    - xyz_i: Matrix with columnds containing cartesian coordinates,
-           relative to the current atom i, i.e.:
-           [[x1 y1 z1]
-            [x2 y2 z2]
-            [x3 y3 z3]
-            [x4 y4 z4]]
-    """
-    xyz = xyz_i
-    r = np.linalg.norm(xyz, axis=1)
-    N = len(r) # Number of neighbors for atom i, which we are currently inspecting
-
-    # A lot of definitions first
-    A = 7.049556277
-    B = 0.6022245584
-    p = 4.
-    q = 0.
-    a = 1.8
-    l = 21. # lambda
-    g = 1.2 # gamma
-    cos_tc = -1.0/3.0 # 109.47 deg
-
-    eps = 2.1683 # [eV]
-    sig = 2.0951 # [Å]
-
-    rc = (r < a*sig) # Bool array. "False" cast to 0 and "True" to 1
-    filterwarnings("ignore", category=RuntimeWarning) # U2 below can give NaN
-    U2 = A*eps*(B*(sig/r)**p-(sig/r)**q) * np.exp(sig/(r-a*sig)) * rc
-    filterwarnings("always", category=RuntimeWarning) # Turn warnings back on
-    def U2_serial(r_vec, r_cut): # Slow, i.e. only use if U2 gives NaN
-        U2_E = 0
-        for r,rc in zip(r_vec,r_cut):
-            if rc:
-                U2_E += A*eps*(B*(sig/r)**p-(sig/r)**q) * np.exp(sig/(r-a*sig))
-            else:
-                pass # Add 0
-        return U2_E
-    def U3(rij, rik, cos_theta):
-        if (rij < a*sig) and (rik < a*sig):
-            exp_factor   = exp(g*sig/(rij-a*sig)) * exp(g*sig/(rik-a*sig))
-            angle_factor = l*eps*(cos_theta - cos_tc)**2
-            return exp_factor * angle_factor
-        else:
-            return 0.0
-    # Sum up two body terms
-    U2_sum  = np.sum(U2) / 2.0 # Each pair share this energy. Contribute half to each
-    if isnan(U2_sum): # NaN gotten, re-computing with serial code. U2 = ", U2_sum
-        U2_sum = U2_serial(r, rc) #/ 2.0
-    # Need a double sum to find three body terms
-    U3_sum = 0.0
-    for j in range(N): # j != i
-        v_rij = xyz[j] # Only depend on j
-        rij   = r[j]
-        for k in range(j+1,N): # i < j < k
-            v_rik = xyz[k]
-            rik   = r[k]
-            cos_theta_jik = np.dot(v_rij,  v_rik) / (rij*rik)
-            U3_sum += U3(rij, rik, cos_theta_jik)
-    U_total = U3_sum + U2_sum/2.0
-    return U_total
 
 def createTrainData(size, neighbors, PES, verbose=False):
     if PES == PES_Stillinger_Weber:
@@ -428,15 +428,6 @@ def testLammpsData(filename):
             xyzr_i   = xyzr_i[:-1].reshape(n_elem,4)
             xyz_i    = xyzr_i[:,:-1]
             Ep2.append(potentialEnergyGenerator(xyz_i,PES=PES_Stillinger_Weber))
-        # import matplotlib.pyplot as plt
-        # plt.subplot(2,1,1)
-        # plt.hist(Ep,bins=200)
-        # plt.subplot(2,1,2)
-        # plt.hist(Ep2,bins=30)
-        # plt.show()
-        # plt.savefig("merkeligEp.pdf")
-        # print len(Ep)
-        # print np.mean(Ep), np.mean(Ep2), np.mean(Ep2)/np.mean(Ep)
 
 def NeighListDataToSymmToFile(open_filename, save_filename, size):
     Ep = []
@@ -543,6 +534,7 @@ def testAngularInvarianceEpAndSymmFuncs():
 if __name__ == '__main__':
     """
     Suggestion: Only have ONE option set to True at the time.
+                (not an absolute rule!)
     """
     # Based on random structures, fixed/variable number of neighbors
     dumpToFile         = False
@@ -552,22 +544,19 @@ if __name__ == '__main__':
     xyz_to_neigh_lists = True
     dumpXYZ_file       = True # Own algo: "readXYZ_Files"
 
-    # Neig.lists from lammps with John-Anders algo
-    dumpLammpsFile     = False
-
     # Unit tests
     testAngSymm        = False
     testLammps         = False
     testClass          = False
 
-    n_atoms    = 3
+    n_atoms    = int(raw_input("Number of atoms? "))
     other_info = "" # i.e. "no_3_body"
     if xyz_to_neigh_lists:
         """
         Takes XYZ files from LAMMPS dump and makes neighbor lists
         """
         cutoff         = 3.77118 # Stillinger-Weber
-        samples_per_dt = "all"   # Integer value or "all"
+        samples_per_dt = 1   # Integer value or "all"
         test_boundary  = False   # Just use atoms wherever they are
         file_path = "Important_data/TestNN/enfil_sw_%sp%s.xyz" %(n_atoms,other_info)
         save_file = "Important_data/neigh_list_from_xyz_%sp%s.txt" %(n_atoms,other_info)
@@ -587,12 +576,6 @@ if __name__ == '__main__':
     if testLammps:
         filename = "Important_data/neighbours.txt"
         testLammpsData(filename)
-
-    if dumpLammpsFile:
-        size          = 10000 # should be <= rows in file!!!!
-        open_filename = "Important_data/neigh_100K.txt"
-        save_filename = "SW_train_lammps_%d_BEH.txt" %size
-        NeighListDataToSymmToFile(open_filename, save_filename, size)
 
     if dumpToFile:
         if True:

derivatives_symm_func.py
@@ -18,9 +18,15 @@ def create_neighbour_list(all_atoms, selfindex, return_self=True):
      [ 3,  4,  5],  -->   [ 0,  0,  0],
      [ 6,  7,  8]]  -->   [ 3,  3,  3]]
     """
-    # TODO: Implement the cutoff
     xyz  = np.copy(all_atoms) # Dont change all_atoms array!
     xyz -= xyz[selfindex,:]
+    # Remove atoms outside SW cutoff! ...by moving them far away
+    for i, pos in enumerate(xyz):
+        if i == selfindex:
+            continue
+        else:
+            if np.linalg.norm(pos) > 3.77118:
+                xyz[i] = np.array([100.,100.,100.]) # Should be far enough away ;)
     if not return_self:
         # Delete row including self atom
         xyz = np.delete(xyz, (selfindex), axis=0)
@@ -28,8 +34,7 @@ def create_neighbour_list(all_atoms, selfindex, return_self=True):
 
 def force_calculation(dNNdG_matrix, all_atoms):
     """
-    Inputs: network, dNNdg, all_atoms
-    - network is an instance of class 'neural network'.
+    Inputs: dNNdg, all_atoms
     - dNNdG_matrix is the matrix composed of vectors of same length as the symmetry vectors.
     It contains the derivative of the neural network with respect to its inputs, i.e.
     the symmetry vector of atom i.
@@ -46,6 +51,15 @@ def force_calculation(dNNdG_matrix, all_atoms):
     tot_atoms   = all_atoms.shape[0]
     all_indices = range(tot_atoms)
 
+    def i2xyz(i):
+        """ For easy reading of error checks """
+        if i == 0:
+            return "x"
+        elif i == 1:
+            return "y"
+        else:
+            return "z"
+
     # Loop over all atoms
     total_forces = np.zeros((tot_atoms, 3)) # Fx, Fy, Fz on all atoms
     for selfindex, atom in enumerate(all_atoms):
@@ -58,6 +72,7 @@ def force_calculation(dNNdG_matrix, all_atoms):
             # Calculating derivative of fingerprints of self atom w.r.t. coordinates of itself.
             sym_f_der = symmetry_func_derivative(selfindex, selfneighborindices, selfneighborpositions, selfindex, i)
             total_forces[selfindex, i] += -np.dot(sym_f_der, self_dNNdG) # Minus sign since F = -d/dx V
+            # print selfindex, i2xyz(i), -np.dot(sym_f_der, self_dNNdG)
 
             # Calculating derivative of fingerprints of neighbor atom w.r.t. coordinates of self atom.
             for neigh_index in selfneighborindices:
@@ -69,6 +84,9 @@ def force_calculation(dNNdG_matrix, all_atoms):
                 # for calculating derivatives of fingerprints, summation runs over neighboring atoms
                 sym_f_der = symmetry_func_derivative(neigh_index, nneighborindices, neighborpositions, selfindex, i)
                 total_forces[selfindex, i] += -np.dot(sym_f_der, neigh_dNNdG)
+        #         print neigh_index, i2xyz(i),-np.dot(sym_f_der, neigh_dNNdG)
+        # print "F sum: X,Y,Z:", total_forces[selfindex]
+        # raw_input("one atom done!")
     return total_forces
 
 
@@ -105,15 +123,13 @@ def symmetry_func_derivative(index, neighborindices, neighborpositions, m, l):
         if which_symm == 2:
             _, eta, rc, rs  = G_funcs[i]
             eta, rc, rs     = float(eta), float(rc), float(rs)
-            ddx_symm_vec[i] = calculate_ddx_G2(neighborindices,
-                                              neighborpositions,
-                                              eta, rc, rs, index, m, l)
+            ddx_symm_vec[i] = calculate_ddx_G2(neighborindices, neighborpositions, eta, rc, rs, index, m, l)
         elif which_symm == 4:
             _, eta, rc, zeta, lamb = G_funcs[i]
             eta, rc, zeta, lamb    = float(eta), float(rc), float(zeta), float(lamb)
-            ddx_symm_vec[i]        = calculate_ddx_G4(neighborindices,
-                                                      neighborpositions,
-                                                      lamb, zeta, eta, rc, index, m, l)
+            ddx_symm_vec[i]        = calculate_ddx_G4(neighborindices, neighborpositions, lamb, zeta, eta, rc, index, m, l)
+            # ddx_symm_vec[i]   = calculate_ddx_G4_own(neighborindices, neighborpositions, lamb, zeta, eta, rc, index, m, l)
+            # raw_input("skip ahead!")
         else:
             print "Only use symmetry functions G2 or G4! Exiting!"
             sys.exit(0)
@@ -172,25 +188,67 @@ def calculate_ddx_G2(neighborindices, neighborpositions, eta, Rc, Rs, i, m, l):
             Rij    = np.linalg.norm(Rj - Ri) # Ri = 0,0,0, but still written for clarity
             term   = -2.0*eta*(Rij-Rs) * cutoff_func(Rij, Rc) + ddx_cutoff_func(Rij, Rc)
             value += exp(-eta*(Rij-Rs)**2) * term * dRijdRml
-        """print "AMP value:", value
-        # Vectors of x,y,z-coordinates for neighbours
-        xij = neighborpositions[j,l]
-        # yij = neighborpositions[j,1]
-        # zij = neighborpositions[j,2]
-        rij    = np.linalg.norm(Rj - Ri) # Ri = 0,0,0, but still written for clarity
-        # rij   = np.linalg.norm((xij, yij, zij)) # Neighbour distance
-        term = (eta*Rc*(rij - Rs)*(cos(pi*rij/Rc) + 1.0) + 0.5*pi*sin(pi*rij/Rc)) \
-               * exp(-eta*(rij - Rs)**2)/(Rc*rij)
-        Fx = -1.0*(xij * term)
-        # Fy = -1.0*(yij * term)
-        # Fz = -1.0*(zij * term)
-        print "OWN Fxyz :", -Fx
-        # print "OWN Fy   :", -Fy
-        # print "OWN Fz   :", -Fz
-        raw_input("\n\n########\n")
-        # return np.array([Fx, Fy, Fz]) # Minus sign comes from F = -d/dx V"""
     return value
 
+def calculate_ddx_G4_own(neighborindices, neighborpositions, lamb, zeta, eta, rc, index, m, l):
+# def calculate_ddx_G4_own(xyz, eta, rc, zeta, lamb):
+    """
+    Derivative of symmetry function G4 w.r.t xij, yij, zij.
+    """
+    # xyz, eta, rc, zeta, lamb
+    xyz = np.array([neighborpositions[ind] for ind in neighborindices])
+    # Vectors of x,y,z-coordinates for neighbours
+    if len(xyz[:,0]) != 2:
+        # print "Warning: dG4 d x,y,z only works on single unique triplet"
+        return np.zeros(3)
+    r = np.linalg.norm(xyz, axis=1) # Neighbour distances
+    xij = xyz[0,0]; yij = xyz[0,1]; zij = xyz[0,2]
+    xik = xyz[1,0]; yik = xyz[1,1]; zik = xyz[1,2]
+    rij = r[0]    ; rik = r[1]    ; rjk = np.linalg.norm(xyz[0,:]-xyz[1,:])
+    cosTheta = (xij*xik + yij*yik + zij*zik) / (rij*rik)
+
+    # ...and finally the derivaties from sympy: (hold on to something!!)
+    dG4_dxij = -2**(-zeta)*(cos(pi*rik/rc) + 1)*(cos(pi*rjk/rc) + 1)\
+               *(0.5*eta*rc*rij**2*rik*xij*(cosTheta*lamb + 1)**(zeta + 1)\
+               *(cos(pi*rij/rc) + 1) + 0.25*lamb*rc*zeta*(cosTheta*lamb + 1)**zeta\
+               *(cosTheta*rik*xij - rij*xik)*(cos(pi*rij/rc) + 1) + 0.25*pi*rij*rik*xij\
+               *(cosTheta*lamb + 1)**(zeta + 1)*sin(pi*rij/rc))*exp(-eta*(rij**2 + rik**2 + rjk**2))\
+               /(rc*rij**2*rik*(cosTheta*lamb + 1))
+    dG4_dyij = -2**(-zeta)*(cos(pi*rik/rc) + 1)*(cos(pi*rjk/rc) + 1)\
+               *(0.5*eta*rc*rij**2*rik*yij*(cosTheta*lamb + 1)**(zeta + 1)\
+               *(cos(pi*rij/rc) + 1) + 0.25*lamb*rc*zeta*(cosTheta*lamb + 1)**zeta\
+               *(cosTheta*rik*yij - rij*yik)*(cos(pi*rij/rc) + 1) + 0.25*pi*rij*rik*yij\
+               *(cosTheta*lamb + 1)**(zeta + 1)*sin(pi*rij/rc))*exp(-eta*(rij**2 + rik**2 + rjk**2))\
+               /(rc*rij**2*rik*(cosTheta*lamb + 1))
+    dG4_dzij = -2**(-zeta)*(cos(pi*rik/rc) + 1)*(cos(pi*rjk/rc) + 1)\
+               *(0.5*eta*rc*rij**2*rik*zij*(cosTheta*lamb + 1)**(zeta + 1)\
+               *(cos(pi*rij/rc) + 1) + 0.25*lamb*rc*zeta*(cosTheta*lamb + 1)**zeta\
+               *(cosTheta*rik*zij - rij*zik)*(cos(pi*rij/rc) + 1) + 0.25*pi*rij*rik*zij\
+               *(cosTheta*lamb + 1)**(zeta + 1)*sin(pi*rij/rc))*exp(-eta*(rij**2 + rik**2 + rjk**2))\
+               /(rc*rij**2*rik*(cosTheta*lamb + 1))
+    dG4_dxik = -2**(-zeta)*(cos(pi*rij/rc) + 1)*(cos(pi*rjk/rc) + 1)\
+               *(0.5*eta*rc*rij*rik**2*xik*(cosTheta*lamb + 1)**(zeta + 1)\
+               *(cos(pi*rik/rc) + 1) + 0.25*lamb*rc*zeta*(cosTheta*lamb + 1)**zeta\
+               *(cosTheta*rij*xik - rik*xij)*(cos(pi*rik/rc) + 1) + 0.25*pi*rij*rik*xik\
+               *(cosTheta*lamb + 1)**(zeta + 1)*sin(pi*rik/rc))*exp(-eta*(rij**2 + rik**2 + rjk**2))\
+               /(rc*rij*rik**2*(cosTheta*lamb + 1))
+    dG4_dyik = -2**(-zeta)*(cos(pi*rij/rc) + 1)*(cos(pi*rjk/rc) + 1)\
+               *(0.5*eta*rc*rij*rik**2*yik*(cosTheta*lamb + 1)**(zeta + 1)\
+               *(cos(pi*rik/rc) + 1) + 0.25*lamb*rc*zeta*(cosTheta*lamb + 1)**zeta\
+               *(cosTheta*rij*yik - rik*yij)*(cos(pi*rik/rc) + 1) + 0.25*pi*rij*rik*yik\
+               *(cosTheta*lamb + 1)**(zeta + 1)*sin(pi*rik/rc))*exp(-eta*(rij**2 + rik**2 + rjk**2))\
+               /(rc*rij*rik**2*(cosTheta*lamb + 1))
+    dG4_dzik = -2**(-zeta)*(cos(pi*rij/rc) + 1)*(cos(pi*rjk/rc) + 1)\
+               *(0.5*eta*rc*rij*rik**2*zik*(cosTheta*lamb + 1)**(zeta + 1)\
+               *(cos(pi*rik/rc) + 1) + 0.25*lamb*rc*zeta*(cosTheta*lamb + 1)**zeta\
+               *(cosTheta*rij*zik - rik*zij)*(cos(pi*rik/rc) + 1) + 0.25*pi*rij*rik*zik\
+               *(cosTheta*lamb + 1)**(zeta + 1)*sin(pi*rik/rc))*exp(-eta*(rij**2 + rik**2 + rjk**2))\
+               /(rc*rij*rik**2*(cosTheta*lamb + 1))
+    tot_force = -1.0*np.array([dG4_dxij+dG4_dxik, dG4_dyij+dG4_dyik, dG4_dzij+dG4_dzik])[l] # Minus sign comes from F = -d/dx V
+    # tot_force = np.array([dG4_dxij, dG4_dyij, dG4_dzij]) # Minus sign comes from F = -d/dx V
+    return tot_force
+    # tot_force = -np.array([dG4_dxij, dG4_dyij, dG4_dzij])[l] # Minus sign comes from F = -d/dx V
+    # return tot_force
 
 def calculate_ddx_G4(neighborindices, neighborpositions, lamb, zeta, eta, cutoff, i, m, l):
     """Calculates coordinate derivative of G4 symmetry function for atom at
@@ -211,10 +269,8 @@ def calculate_ddx_G4(neighborindices, neighborpositions, lamb, zeta, eta, cutoff
         Parameter of Behler symmetry functions.
     eta : float
         Parameter of Behler symmetry functions.
-    cutoff : dict
-        Cutoff function, typically from amp.descriptor.cutoffs. Should be also
-        formatted as a dictionary by todict method, e.g.
-        cutoff=Cosine(6.5).todict()
+    cutoff : float
+        Interaction distance
     i : int
         Index of the center atom.
     Ri : numpy array
@@ -236,15 +292,14 @@ def calculate_ddx_G4(neighborindices, neighborpositions, lamb, zeta, eta, cutoff
     ddx_cutoff_func = ddx_cutoff_cos
     value           = 0.0
     # number of neighboring atoms
-    # counts = range(len(neighborpositions))
-    for jj,j in enumerate(neighborindices): # J = index of atom j, jj = index of j in neigh.indices
-        for k in neighborindices:#[jj+1:]:
+    for j in neighborindices: # J = index of atom j
+        Rj = neighborpositions[j]
+        Rij_vector = neighborpositions[j] - Ri
+        Rij = np.linalg.norm(Rij_vector)
+        for k in neighborindices:
             if j == k:
                 continue
-            Rj = neighborpositions[j]
             Rk = neighborpositions[k]
-            Rij_vector = neighborpositions[j] - Ri
-            Rij = np.linalg.norm(Rij_vector)
             Rik_vector = neighborpositions[k] - Ri
             Rik = np.linalg.norm(Rik_vector)
             Rjk_vector = neighborpositions[k] - neighborpositions[j]
@@ -255,14 +310,14 @@ def calculate_ddx_G4(neighborindices, neighborpositions, lamb, zeta, eta, cutoff
             fcRij = cutoff_func(Rij, Rc)
             fcRik = cutoff_func(Rik, Rc)
             fcRjk = cutoff_func(Rjk, Rc)
-            if zeta == 1:
-                term1 = exp(- eta * (Rij ** 2 + Rik ** 2 + Rjk ** 2))
-            else:
-                term1 = c1 ** (zeta - 1.0) * \
-                    exp(- eta * (Rij ** 2 + Rik ** 2 + Rjk ** 2))
-            term2 = 0.0
             fcRijfcRikfcRjk = fcRij * fcRik * fcRjk
-            dCosthetadRml   = dCos_theta_ijk_dR_ml(i, j, k, Ri, Rj, Rk, m, l)
+
+            if zeta == 1:
+                term1 = exp(-eta*(Rij**2 + Rik**2 + Rjk**2))
+            else:
+                term1 = exp(-eta*(Rij**2 + Rik**2 + Rjk**2)) * c1**(zeta-1.0)
+            term2 = 0.0
+            dCosthetadRml = dCos_theta_ijk_dR_ml(i, j, k, Ri, Rj, Rk, m, l)
             if dCosthetadRml != 0:
                 term2 += lamb * zeta * dCosthetadRml
             dRijdRml = dRij_dRml(i, j, Ri, Rj, m, l)
@@ -331,7 +386,7 @@ def dCos_theta_ijk_dR_ml(i, j, k, Ri, Rj, Rk, m, l):
 
     dRijdRml = dRij_dRml(i, j, Ri, Rj, m, l)
     if dRijdRml != 0:
-        dCos_theta_ijk_dR_ml += - np.dot(Rij_vector, Rik_vector) * dRijdRml / ((Rij ** 2.) * Rik)
+        dCos_theta_ijk_dR_ml += -np.dot(Rij_vector, Rik_vector) * dRijdRml / ((Rij ** 2.) * Rik)
 
     dRikdRml = dRij_dRml(i, k, Ri, Rk, m, l)
     if dRikdRml != 0:
@@ -409,7 +464,7 @@ def ddx_cutoff_cos(Rij, Rc):
     float
         The vaule of derivative of the cutoff function.
     """
-    if Rij > Rc:
+    if Rij > Rc:# or Rij > 3.77118:
         return 0.
     else:
         return (-0.5 * pi / Rc) * sin(pi * Rij / Rc)
@@ -436,14 +491,14 @@ def dRij_dRml(i, j, Ri, Rj, m, l):
     Returns
     -------
     dRij_dRml : list of float
-        The derivative of the noRi of position vector R_{ij} with respect to
+        The derivative of the Ri of position vector R_{ij} with respect to
         x_{l} of atomic index m.
     """
     Rij = np.linalg.norm(Rj - Ri)
     if m == i:
         dRij_dRml = -(Rj[l] - Ri[l]) / Rij
     elif m == j:
-        dRij_dRml = (Rj[l] - Ri[l]) / Rij
+        dRij_dRml =  (Rj[l] - Ri[l]) / Rij
     else:
         dRij_dRml = 0
     return dRij_dRml
@@ -578,3 +633,12 @@ def cos_theta_check(cos_theta):
 #     Fy = -1.0*np.sum(-yij * term)
 #     Fz = -1.0*np.sum(-zij * term)
 #     return np.array([Fx, Fy, Fz])
+
+if __name__ == '__main__':
+    """
+    Checking that neigh. list correctly removes atoms outside cutoff at 3.77118
+    """
+    test = np.array([[1, 1, 1], [2, 2, 2], [3, 3, 3], [4, 4, 4]])
+    for selfindex in range(4):
+        print selfindex
+        print create_neighbour_list(test, selfindex), "\n"

file_management.py
@@ -4,6 +4,7 @@ import glob
 import shutil
 import datetime
 import numpy as np
+from time import sleep
 
 def timeStamp():
     return datetime.datetime.now().strftime("%H.%M.%S--%d.%m.%Y")
@@ -170,7 +171,8 @@ def readXYZ_Files(path_to_file, save_name, samples_per_dt=30, cutoff=3.77118,
     """
     print "\nReading XYZ-file:"
     print '"%s"' %path_to_file
-    print "NB: This might take some minutes, depending on the number of samples and time steps!"
+    print "NB: This might take some minutes, dep. on the samples / time step!"
+    sleep(1.0)
     if samples_per_dt == "all":
         return_all     = True
     else:
@@ -186,11 +188,12 @@ def readXYZ_Files(path_to_file, save_name, samples_per_dt=30, cutoff=3.77118,
                 if row == 0:
                     tot_nmbr_of_atoms = int(line)
                     xyz_ti = np.zeros((tot_nmbr_of_atoms, 3))
-                    print "Number of atoms:", tot_nmbr_of_atoms
-                    samples_per_dt = int(tot_nmbr_of_atoms)
+                    # print "Number of atoms:", tot_nmbr_of_atoms
+                    if samples_per_dt == "all":
+                        samples_per_dt = int(tot_nmbr_of_atoms)
                     continue
                 elif row == 1:
-                    print 'Comment line said: "%s"' %line[:-1]
+                    # print 'Comment line said: "%s"' %line[:-1]
                     continue
             elif row == 0 or row == 1:
                 continue
@@ -288,8 +291,6 @@ def compute_neigh_lists(xyz, master_neigh_list, samples_per_dt, cutoff, test_bou
         nn_list.append("nan") # This file does not contain pot. energy. So if wrongly read, give NAN
         master_neigh_list.append(nn_list)
         if i == tot_neig:
-            # for i in master_neigh_list:
-            #     print np.array(i),"\n"
             break
 
 

force_verify.py
@@ -26,7 +26,7 @@ def test_structure_N_atom(neigh_cube, neural_network, plot_single=False, last_ti
 
     # Need Ep of all atoms in NN-calculation of forces:
     tot_nmbr_of_atoms = neigh_cube[0].shape[0]
-    Ep_NN_all_atoms   = np.zeros(tot_nmbr_of_atoms)
+    Ep_NN_all_atoms   = [None]*3
     _, nmbr_G         = generate_symmfunc_input_Si_Behler()
     dNNdG_matrix      = np.zeros((tot_nmbr_of_atoms, nmbr_G))
 
@@ -44,29 +44,46 @@ def test_structure_N_atom(neigh_cube, neural_network, plot_single=False, last_ti
         Fvec_SW = (0,0,0) # Only a placeholder! LAMMPS data filled in later
 
         # Potential and forces computed by trained neural network:
-        for i_atom in range(tot_nmbr_of_atoms):
-            xyz_atom_centered       = create_neighbour_list(xyz, i_atom, return_self=False)
-            symm_vec                = neural_network.create_symvec_from_xyz(xyz_atom_centered)
-            Ep_NN_all_atoms[i_atom] = neural_network(symm_vec) # Evaluates the NN
-            dNNdG_matrix[i_atom,:]  = neural_network.derivative().reshape(nmbr_G,)
-        # Now that we have all Ep of all atoms, run force calculation:
-        f_tot = force_calculation(dNNdG_matrix, xyz)
+        # for i_atom in range(tot_nmbr_of_atoms):
+        #     xyz_atom_centered       = create_neighbour_list(xyz, i_atom, return_self=False)
+        #     symm_vec                = neural_network.create_symvec_from_xyz(xyz_atom_centered)
+        #     Ep_NN_all_atoms[i_atom] = neural_network(symm_vec) # Evaluates the NN
+        #     dNNdG_matrix[i_atom,:]  = neural_network.derivative().reshape(nmbr_G,)
+        # # Now that we have all Ep of all atoms, run force calculation:
+        # f_tot = force_calculation(dNNdG_matrix, xyz)
+
+        # Finite difference derivative of NN:
+        i_a       = 0 # Look at this particle only
+        off_value = 0.001
+        num_force_atom_0 = [0,0,0]
+        for fdir in [0,1,2]: # Force in direction x, y, z
+            Ep_off = [0,0] # Reset Ep
+            for i_off, offset in enumerate([-off_value, off_value]):
+                xyz_c            = np.copy(xyz)
+                xyz_c[i_a,fdir] -= offset # Moving the atom a tiny bit in direction "fdir"
+                for cur_atom in range(tot_nmbr_of_atoms):
+                    xyz_atom_centered = create_neighbour_list(xyz_c, cur_atom, return_self=False)
+                    symm_vec          = neural_network.create_symvec_from_xyz(xyz_atom_centered)
+                    Ep_off[i_off]    += neural_network(symm_vec) # Evaluates the NN
+            # Compute the force with central difference (Error: O(dx^2)) <-- big O-notation
+            num_force_atom_0[fdir] = (Ep_off[1]-Ep_off[0])/(2*off_value)
 
         # Append all values to lists:
         Ep_SW_list.append(Ep_SW)
-        Ep_NN_list.append(Ep_NN_all_atoms[0]) # Pick out first atom (for comparison)
+        Ep_NN_list.append(Ep_NN_all_atoms[:]) # Pick out first atom (for comparison)
         Fvec_SW_list.append(Fvec_SW)
-        Fvec_NN_list.append(f_tot[0])         # Pick out first atom (for comparison)
+        # Fvec_NN_list.append(f_tot[0])
+        Fvec_NN_list.append(num_force_atom_0)
 
         # Print out progress
         if t%20 == 0 and t > 50:
             sys.stdout.write("\rTimestep: %d" %t)
             sys.stdout.flush()
     print " "
-    # sys.exit(0) # """#########################"""
-
+    Ep_SW_list = np.array(Ep_SW_list)
+    Ep_NN_list = np.array(Ep_NN_list)
     if plot_single:
-        plotErrorEvolutionSWvsNN(Ep_SW_list, Ep_NN_list, tot_nmbr_of_atoms)
+        plotErrorEvolutionSWvsNN(Ep_SW_list, Ep_NN_list[:,0], tot_nmbr_of_atoms)
     # Return values for more plotting
     return Ep_SW_list, Ep_NN_list, tot_nmbr_of_atoms, Fvec_SW_list, Fvec_NN_list
 
@@ -81,7 +98,8 @@ if __name__ == '__main__':
         print "- N is the different NN-versions to visualize"
         print "- M is the last timestep"
         sys.exit(0)
-    path_to_file = "Important_data/TestNN/enfil_sw_3p.xyz"
+    n_atoms    = int(raw_input("Number of atoms? "))
+    path_to_file = "Important_data/TestNN/enfil_sw_%dp.xyz" %n_atoms
     neigh_cube   = readXYZ_Files(path_to_file, "no-save-file.txt", return_array=True)
     loadPath     = findPathToData(find_tf_savefile=True)
     master_list  = []
@@ -105,16 +123,30 @@ if __name__ == '__main__':
         nn_eval = neural_network(loadPath, sigmoid, ddx_sig)
         Ep_SW, Ep_NN, N_atoms, F_SW, F_NN = test_structure_N_atom(neigh_cube,
                                             nn_eval, plot_single=False, last_timestep=last_timestep)
-        plot_info = [Ep_SW, Ep_NN, N_atoms, nn_eval.what_epoch]
+        # diff = np.mean(np.abs(np.array([i-j for i,j in zip(Ep_SW, Ep_NN[:,0])])))
+        # print "Potential energy abs diff:", diff
+        plot_info = [Ep_SW, Ep_NN[:,0], N_atoms, nn_eval.what_epoch]
         master_list.append(plot_info)
 
     # Plot each epoch in a new subplot:
     if N > 1:
         plotEvolutionSWvsNN_N_diff_epochs(N, master_list)
 
-    # Grab LAMMPS force data
-    # NB!!!
+
+    # Pick out first atom (for comparison)
+    F_NN = np.array(F_NN)#[:,0]
+
+    # Grab LAMMPS force data, NB: must be perfectly consistent with XYZ-files!!!!!
     F_LAMMPS = plotLAMMPSforces1atomEvo()[:len(F_NN)]
+    F_SW     = np.array(F_LAMMPS, dtype=float)
 
     # Plot comparison of forces
     plotForcesSWvsNN(F_LAMMPS, F_NN, show=True)
+    # plotForcesSWvsNN(F_LAMMPS, np.array(F_NN)[:,0], show=True)
+    # plotForcesSWvsNN(F_LAMMPS, np.array(F_NN)[:,1], show=True)
+    # plotForcesSWvsNN(F_LAMMPS, np.array(F_NN)[:,2], show=True)
+    # for i in range(last_timestep):
+    #     print "Forces LAMMPS:", F_SW[i]
+    #     print "Forces, NN   :", F_NN[i]
+    #     print "Diff,        :", F_SW[i]-F_NN[i], "\n"
+    #     raw_input("ASDF")

symmetry_functions.py
@@ -11,7 +11,6 @@ def cutoff_tanh(r,rc):
     """
     Can take scalar and vector input of r and evaluate the cutoff function
     """
-    # rc = float(rc)
     if type(r) == int:
         if r <= rc:
             return tanh(1-r/rc)**3
@@ -24,14 +23,14 @@ def cutoff_cos(r,rc):
     """
     Can take scalar, vector or matrix input of r and evaluate the cutoff function
     """
-    # rc = float(rc)
+    # r_SW_cut = 100#3.77118
     if type(r) == int:
-        if r <= rc:
+        if r <= rc and r < r_SW_cut:
             return 0.5*(cos(pi*r/rc)+1)
         else:
             return 0.
     else:
-        return 0.5*(np.cos(pi*r/rc)+1) * (r <= rc)
+        return 0.5*(np.cos(pi*r/rc)+1) * (r <= rc)# * (r < r_SW_cut)
 
 """
 #################
@@ -161,12 +160,12 @@ def G4_single_neighbor_radial_cut(r, rc, zeta, lambda_c, eta, cutoff=cutoff_cos)
     angle_factor = 2**(1-zeta) * (1 + lambda_c * np.cos(theta))**zeta
     return angle_factor * exp_factor * cutoff(r, rc)**3
 
-def G5_single_neighbor_radial_cut(r, rc, zeta, lambda_c, eta, rs, cutoff=cutoff_cos):
+def G5_single_neighbor_radial_cut(r, rc, zeta, lambda_c, eta, cutoff=cutoff_cos):
     """
     With cutoff
     """
     theta = pi/3. # Constant at 60 degrees aka pi/3
-    exp_factor   = np.exp(-eta * 2*(r-rs)**2)
+    exp_factor   = np.exp(-eta * 2*r**2)
     angle_factor = 2**(1-zeta) * (1 + lambda_c * cos(theta))**zeta
     return angle_factor * exp_factor * cutoff(r, rc)**2
 